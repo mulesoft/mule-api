98ce210c4a4e356bcbb74ffab797ce0ffcd63e5b
alegmarra <alemarra@gmail.com>
2016-06-22 12:17:04 -0300

    refactor attribtues resolver

--- src/main/java/org/mule/runtime/api/metadata/resolving/MetadataAttributesResolver.java
+++ src/main/java/org/mule/runtime/api/metadata/resolving/MetadataAttributesResolver.java
@@ -18,7 +18,7 @@ import org.mule.runtime.api.metadata.MetadataResolvingException;
  * @param <K> the generic type of the objects to be used as MetadataKeyId representing a type structure
  * @since 1.0
  */
-public interface MetadataOutputResolverWithAttributes<K> extends MetadataOutputResolver<K>
+public interface MetadataAttributesResolver<K>
 {
 
     /**
