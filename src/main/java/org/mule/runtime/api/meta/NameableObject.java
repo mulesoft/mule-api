a064ad5f542001be7a26ca06ff6e3ed8b91b6615
Pablo Kraan <pablokraan@users.noreply.github.com>
2016-07-21 13:27:50 -0300

    MULE-10139: Move NamedObject, NameableObject and AnnotatedObject to mule-api (#52)

---
+++ src/main/java/org/mule/runtime/api/meta/NameableObject.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.meta;
+
+/**
+ * Adds {@link #getName} and {@link #setName} methods to an object
+ *
+ * @since 1.0
+ */
+public interface NameableObject extends NamedObject
+{
+
+    /**
+     * Sets the name of the object
+     *
+     * @param name the name of the object
+     */
+    void setName(String name);
+}
