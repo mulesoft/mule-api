df38e14405bc47e5714949659667c990a52d3afb
Pablo Musumeci <pablo.musumeci@mulesoft.com>
2016-08-19 15:00:46 -0300

    MULE-10260: Add endpoint for retrieving keys by config or resolver

--- src/main/java/org/mule/runtime/api/metadata/MetadataManager.java
+++ src/main/java/org/mule/runtime/api/metadata/MetadataManager.java
@@ -13,56 +13,65 @@ import org.mule.runtime.api.metadata.resolving.MetadataKeysResolver;
 import org.mule.runtime.api.metadata.resolving.MetadataOutputResolver;
 import org.mule.runtime.api.metadata.resolving.MetadataResult;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
- * Provides access to the Metadata of any {@link MetadataAware} Component in the application,
- * using it's {@link ComponentId}
+ * Provides access to the Metadata of any {@link MetadataAware} Component in the application, using it's {@link ComponentId} It
+ * also provides access to the {@link MetadataKey} associated to a configuration through the operations and sources that belong to
+ * that configuration.
  *
  * @since 1.0
  */
 public interface MetadataManager {
 
   /**
-   * Returns the list of types that can be described by the {@link MetadataKeysResolver}
-   * associated to the {@link MetadataAware} Component identified by the {@link ComponentId}.
+   * Returns the list of types that can be described by the {@link MetadataKeysResolver} associated to the {@link MetadataAware}
+   * Component identified by the {@link ComponentId}.
    *
    * @param componentId the id of the {@link MetadataAware} component to query for its available keys
-   * @return Successful {@link MetadataResult} if the keys are successfully resolved
-   * Failure {@link MetadataResult} if there is an error while resolving the keys
+   * @return Successful {@link MetadataResult} if the keys are successfully resolved Failure {@link MetadataResult} if there is an
+   *         error while resolving the keys
    */
   MetadataResult<Set<MetadataKey>> getMetadataKeys(ComponentId componentId);
 
   /**
-   * Resolves the {@link ComponentMetadataDescriptor} for the {@link MetadataAware} Component
-   * identified by the {@link ComponentId} using only the static types of its parameters, attributes and output.
+   * Returns a map with the resolvers associated to the configuration named {@code configName} as keys, and a {@link Set} of
+   * {@link MetadataKey} as the values for that particular config.
+   *
+   * @param configName the name of the {@link MetadataKeyAware} component to query for its available keys
+   * @return Successful {@link MetadataResult} if the keys are successfully resolved Failure {@link MetadataResult} if there is an
+   *         error while resolving the keys
+   */
+  MetadataResult<Map<String, Set<MetadataKey>>> getMetadataKeys(String configName);
+
+  /**
+   * Resolves the {@link ComponentMetadataDescriptor} for the {@link MetadataAware} Component identified by the
+   * {@link ComponentId} using only the static types of its parameters, attributes and output.
    *
    * @param componentId the id of the {@link MetadataAware} component to query for its available keys
-   * @return An {@link ComponentMetadataDescriptor} with the static Metadata representation
-   * of the Component.
-   * Successful {@link MetadataResult} if the Metadata is successfully retrieved
-   * Failure {@link MetadataResult} when the Metadata retrieval of any element fails for any reason
+   * @return An {@link ComponentMetadataDescriptor} with the static Metadata representation of the Component. Successful
+   *         {@link MetadataResult} if the Metadata is successfully retrieved Failure {@link MetadataResult} when the Metadata
+   *         retrieval of any element fails for any reason
    */
   MetadataResult<ComponentMetadataDescriptor> getMetadata(ComponentId componentId);
 
   /**
-   * Resolves the {@link ComponentMetadataDescriptor} for the {@link MetadataAware} Component
-   * identified by the {@link ComponentId} using both static and dynamic resolving of
-   * the parameters, attributes and output types.
+   * Resolves the {@link ComponentMetadataDescriptor} for the {@link MetadataAware} Component identified by the
+   * {@link ComponentId} using both static and dynamic resolving of the parameters, attributes and output types.
    * <p>
-   * If the component has a {@link MetadataContentResolver} or {@link MetadataOutputResolver} associated
-   * that can be used to resolve the dynamic {@link MetadataType} for the Content or Output,
-   * then the {@link ComponentMetadataDescriptor} will contain those dynamic types instead
-   * of the static types declaration.
+   * If the component has a {@link MetadataContentResolver} or {@link MetadataOutputResolver} associated that can be used to
+   * resolve the dynamic {@link MetadataType} for the Content or Output, then the {@link ComponentMetadataDescriptor} will contain
+   * those dynamic types instead of the static types declaration.
    * <p>
-   * When neither Content nor Output have dynamic types, then invoking this method is the
-   * same as invoking {@link this#getMetadata}
+   * When neither Content nor Output have dynamic types, then invoking this method is the same as invoking
+   * {@link this#getMetadata}
    *
    * @param componentId the id of the {@link MetadataAware} component to query for its available keys
-   * @param key         {@link MetadataKey} of the type which's structure has to be resolved, used both for input and output types
-   * @return a {@link MetadataResult} of {@link ComponentMetadataDescriptor} type with Successful {@link MetadataResult}
-   * if the Metadata is successfully retrieved and a Failed {@link MetadataResult} when the Metadata retrieval of any
-   * element fails for any reason
+   * @param key {@link MetadataKey} of the type which's structure has to be resolved, used both for input and output types
+   * @return a {@link MetadataResult} of {@link ComponentMetadataDescriptor} type with Successful {@link MetadataResult} if the
+   *         Metadata is successfully retrieved and a Failed {@link MetadataResult} when the Metadata retrieval of any element
+   *         fails for any reason
    */
   MetadataResult<ComponentMetadataDescriptor> getMetadata(ComponentId componentId, MetadataKey key);
 
