110711dee3ea4d69afb4bf5c7cd6b1b34dc6c425
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-09-05 22:03:03 +0200

    MULE-10462: Remove old DB connector from distribution (#72)

---
+++ src/main/java/org/mule/runtime/api/config/DatabasePoolingProfile.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.config;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Pooling configuration for JDBC Data Sources capable of
+ * pooling connections.
+ *
+ * Unlike {@link PoolingProfile} which serves a more general
+ * purpose, this interface should be used when configuring
+ * pooling for JDBC connections
+ *
+ * @since 1.0
+ */
+//TODO: MULE-9047 - review if we can use the standard pooling profile
+public interface DatabasePoolingProfile {
+
+  /**
+   * @return Maximum number of connections a pool maintains at any given time
+   */
+  int getMaxPoolSize();
+
+  /**
+   * @return Minimum number of connections a pool maintains at any given time
+   */
+  int getMinPoolSize();
+
+  /**
+   * @return How many connections to acquire at a time when pool is exhausted
+   */
+  int getAcquireIncrement();
+
+  /**
+   * @return How many statements are cached per pooled connection. Defaults to 0, meaning statement caching is disabled
+   */
+  int getPreparedStatementCacheSize();
+
+  /**
+   * @return The number of milliseconds a client trying to obtain a connection waits for
+   * it to be checked-in or acquired when the pool is exhausted. Zero (default) means wait indefinitely
+   */
+  int getMaxWait();
+
+  /**
+   * @return A {@link TimeUnit} which qualifies the {@link #getMaxWait()}.
+   */
+  TimeUnit getMaxWaitUnit();
+}
---
+++ src/main/java/org/mule/runtime/api/tx/DataSourceDecorator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.tx;
+
+import org.mule.runtime.api.config.DatabasePoolingProfile;
+
+import javax.sql.DataSource;
+
+/**
+ * Decorates {@link DataSource} if required in order to work with XA transactions
+ * @since 1.0
+ */
+public interface DataSourceDecorator {
+
+  /**
+   * Decorates a dataSource
+   *
+   * @param dataSource dataSource to decorate. Non null
+   * @param dataSourceName dataSource bean name
+   * @param dbPoolingProfile pooling profile use to create the wrapped dataSource
+   * @return
+   */
+  DataSource decorate(DataSource dataSource, String dataSourceName, DatabasePoolingProfile dbPoolingProfile);
+
+  /**
+   * Indicates whether or not this decorator can decorate a given datasource
+   *
+   * @param dataSource dataSource to check
+   * @return whether the {@code dataSource} can be decorated by {@code this} instance
+   */
+  boolean appliesTo(DataSource dataSource);
+
+}
