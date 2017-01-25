d372a665971d4c293b51e87a9b6d34a17b030c95
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-01-25 18:57:26 -0300

    MULE-11459: Introduce Cursor Streams

--- src/main/java/org/mule/runtime/api/streaming/CursorStream.java
+++ src/main/java/org/mule/runtime/api/streaming/CursorStream.java
@@ -12,12 +12,20 @@ import java.io.InputStream;
 /**
  * An {@link InputStream} which provides random access.
  * <p>
- * It works by adding the concept of a zero-base position. Each position
+ * It works by adding the concept of a zero-based position. Each position
  * represents one byte in the stream. Each time data is pulled from this stream,
  * the position advances as many bytes as read. However, the {@link #seek(long)}
  * method can be used to reset the position.
  * <p>
- * This is not thread safe. Should not be used concurrently
+ * This streams are auto-closeable in the sense that once a read operation returns {@code -1},
+ * the {@link #isClosed()} method will start to return {@core true}. However, an invokation to
+ * {@link #seek(long)} will mark {@code this} cursor as open again.
+ * <p>
+ * Eventually, the runtime (and only the runtime) will dispose of this cursor, releasing the
+ * resources it has allocated. Once that happens, {@link #isClosed()} will return {@code true}
+ * again but no action will move it from that state.
+ * <p>
+ * This is not thread safe. Should not be used concurrently.
  *
  * @since 1.0
  */
--- src/main/java/org/mule/runtime/api/streaming/CursorStreamProvider.java
+++ src/main/java/org/mule/runtime/api/streaming/CursorStreamProvider.java
@@ -6,57 +6,25 @@
  */
 package org.mule.runtime.api.streaming;
 
-import static java.util.Collections.newSetFromMap;
-import static org.mule.runtime.api.util.Preconditions.checkState;
-
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * An {@link InputStream} decorator which supports concurrent random access.
+ * Provides instances of {@link CursorStream} which allows concurrent access of a wrapped
+ * {@link InputStream}.
  * <p>
- * This class works by buffering an existing {@link InputStream}. This buffering allows for data
- * to be kept so that random access is allowed. The size of the buffer is configured through the
- * constructor. Upon creation, the wrapped stream will be consumed until the buffer is full.
- * If the wrapped stream is fully consumed before the buffer is full, then those contents will be
- * kept in memory for fast random access. If the buffer gets full before the stream is fully consumed,
- * then a temporal file will be used to buffer the contents.
+ * The provider maintains all the allocated resources necessary to power all the open
+ * cursors.
  * <p>
- * Regardless of the buffering strategy, safe concurrent access will be guaranteed.
- * <p>
- * Although this decorator is perfectly functional as a stream, the {@link #openCursor()} method
- * should be used in order to support random concurrent access. That method creates a new
- * {@link CursorStream} which is linked to this instance and leverages the buffer. It allows different clients
- * to reference different positions on the buffer and access them in a thread safe manner.
- * <p>
- * This stream is not closed until all open cursors are closed. This makes it critical for anyone invoking the
- * {@link #openCursor()} method to make sure that the cursor is closed when no longer needed, since otherwise
- * any resources held by the underlying buffer will never be released.
+ * When this provider is {@link #close() closed}, it will not open any new cursors
+ * and invokations to {@link #openCursor()} will fail. However, the allocated resources
+ * will not be released until all open cursors are closed. The runtime will be in charge of
+ * doing that, no intervention is required from the client.
  *
  * @see CursorStream
  * @since 1.0
  */
-public abstract class CursorStreamProvider {
+public interface CursorStreamProvider {
 
-  protected final InputStream wrappedStream;
-  protected final int bufferSize;
-  private final Set<CursorStream> cursors = newSetFromMap(new ConcurrentHashMap<>());
-
-  private AtomicBoolean closed = new AtomicBoolean(false);
-
-  /**
-   * Creates a new instance
-   *
-   * @param wrappedStream the original stream to be decorated
-   * @param bufferSize    the size in bytes of the buffer
-   */
-  public CursorStreamProvider(InputStream wrappedStream, int bufferSize) {
-    this.wrappedStream = wrappedStream;
-    this.bufferSize = bufferSize;
-  }
 
   /**
    * Creates a new {@link CursorStream} positioned on the very beginning of the wrapped stream.
@@ -71,103 +39,18 @@ public abstract class CursorStreamProvider {
    * @return a new {@link CursorStream}
    * @throws IllegalStateException if invoked after the {@link #close()} method has been invoked
    */
-  public final CursorStream openCursor() {
-    checkState(!closed.get(), "Cannot open a new cursor on a closed stream");
-    CursorStream cursor = doOpenCursor();
-    cursors.add(cursor);
+  CursorStream openCursor();
 
-    return new ManagedCursorStreamDecorator(cursor);
-  }
+  /**
+   * Closes the provider in the sense that it will not yield any more cursors. However, all active
+   * cursors will remain functional. When the last of them is closed, then any resources associated to {@code this}
+   * provider will be released.
+   */
+  void close();
 
-  public void close() {
-    closed.set(true);
-  }
+  /**
+   * @return Whether the {@link #close()} method has been invoked on {@code this} instance or not
+   */
+  boolean isClosed();
 
-  protected abstract void releaseResources();
-
-  protected abstract CursorStream doOpenCursor();
-
-  private void closeCursor(CursorStream cursor) {
-    cursors.remove(cursor);
-    if (closed.get() && allCursorsClosed()) {
-      releaseResources();
-    }
-  }
-
-  private boolean allCursorsClosed() {
-    return cursors.isEmpty();
-  }
-
-  private class ManagedCursorStreamDecorator extends CursorStream {
-
-    private final CursorStream delegate;
-
-    private ManagedCursorStreamDecorator(CursorStream delegate) {
-      this.delegate = delegate;
-    }
-
-    @Override
-    public void close() throws IOException {
-      try {
-        delegate.close();
-      } finally {
-        closeCursor(delegate);
-      }
-    }
-
-    @Override
-    public long getPosition() {
-      return delegate.getPosition();
-    }
-
-    @Override
-    public void seek(long position) throws IOException {
-      delegate.seek(position);
-    }
-
-    @Override
-    public boolean isClosed() {
-      return delegate.isClosed();
-    }
-
-    @Override
-    public int read() throws IOException {
-      return delegate.read();
-    }
-
-    @Override
-    public int read(byte[] b) throws IOException {
-      return delegate.read(b);
-    }
-
-    @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-      return delegate.read(b, off, len);
-    }
-
-    @Override
-    public long skip(long n) throws IOException {
-      return delegate.skip(n);
-    }
-
-    @Override
-    public int available() throws IOException {
-      return delegate.available();
-    }
-
-    @Override
-    public void mark(int readlimit) {
-      delegate.mark(readlimit);
-    }
-
-    @Override
-    public void reset() throws IOException {
-      delegate.reset();
-    }
-
-    @Override
-    public boolean markSupported() {
-      return delegate.markSupported();
-    }
-  }
 }
