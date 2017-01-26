d19fe9a2dad6077be1715845eefd847287d2d4ca
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2017-01-26 18:49:39 -0300

    MULE-11540: Have an ExpressionManager interface in API

---
+++ src/main/java/org/mule/runtime/api/el/ExpressionEvaluator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.el;
+
+import org.mule.runtime.api.metadata.TypedValue;
+
+
+/**
+ * Provides universal access for evaluating expressions.
+ * 
+ * @since 1.0
+ */
+public interface ExpressionEvaluator {
+
+  /**
+   * Evaluates an expression according to a given {@link BindingContext} and the global one.
+   *
+   * @param expression the EL expression
+   * @param context the current dynamic binding context to consider
+   * @return the result of the expression plus its type
+   * @throws ExpressionExecutionException when an error occurs during evaluation
+   */
+  TypedValue evaluate(String expression, BindingContext context) throws ExpressionExecutionException;
+
+  /**
+   * Verifies whether an expression is valid or not syntactically.
+   *
+   * @param expression to be validated
+   * @return a {@link ValidationResult} indicating whether the validation was successful or not
+   */
+  ValidationResult validate(String expression);
+
+}
