b51f64b11d6881317814ec948c7a67759898911a
Ana Felisatti <afelisatti@mulesoft.com>
2017-04-05 14:28:04 -0300

    Update DataType.java

--- src/main/java/org/mule/runtime/api/metadata/DataType.java
+++ src/main/java/org/mule/runtime/api/metadata/DataType.java
@@ -71,7 +71,7 @@ public interface DataType extends Serializable {
    * {@code mimeType} and {@code encoding} values based on this.
    *
    * @param value the object to determine the {@link DataType} of.
-   * @return a new {@link DataType} for the given {@code valuet}.
+   * @return a new {@link DataType} for the given {@code value}.
    */
   static DataType fromObject(Object value) {
     return getDefaultFactory().create().fromObject(value).build();
