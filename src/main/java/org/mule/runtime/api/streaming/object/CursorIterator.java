100a029c605fb94d553b3a6c17702dce6b3a614f
Pablo Musumeci <pablo.musumeci@mulesoft.com>
2017-05-11 15:33:53 -0300

    MULE-12158: Auto paging and cursor iterators should have a getSize()

--- src/main/java/org/mule/runtime/api/streaming/object/CursorIterator.java
+++ src/main/java/org/mule/runtime/api/streaming/object/CursorIterator.java
@@ -7,7 +7,7 @@
 package org.mule.runtime.api.streaming.object;
 
 import org.mule.runtime.api.streaming.Cursor;
-import org.mule.runtime.api.streaming.Sized;
+import org.mule.runtime.api.streaming.HasSize;
 
 import java.util.Iterator;
 
@@ -16,6 +16,6 @@ import java.util.Iterator;
  *
  * @since 1.0
  */
-public interface CursorIterator<T> extends Iterator<T>, Cursor, Sized {
+public interface CursorIterator<T> extends Iterator<T>, Cursor, HasSize {
 
 }
