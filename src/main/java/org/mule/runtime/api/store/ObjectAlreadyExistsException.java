00a7664a68777aa7b662ba4659d7169ab3c36a51
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-05-16 22:46:52 +0200

    MULE-12494: Move ObjectStore to Mule API (#222)

---
+++ src/main/java/org/mule/runtime/api/store/ObjectAlreadyExistsException.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.store;
+
+import org.mule.runtime.api.i18n.I18nMessage;
+
+/**
+ * This exception is thrown when an {@link ObjectStore} tries to store a value using a key for which another value
+ * is already present.
+ *
+ * @since 1.0
+ */
+public class ObjectAlreadyExistsException extends ObjectStoreException {
+
+  public ObjectAlreadyExistsException() {
+    super();
+  }
+
+  public ObjectAlreadyExistsException(I18nMessage message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ObjectAlreadyExistsException(I18nMessage message) {
+    super(message);
+  }
+
+  public ObjectAlreadyExistsException(Throwable cause) {
+    super(cause);
+  }
+}
+
+
---
+++ src/main/java/org/mule/runtime/api/store/ObjectStoreNotAvailableException.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.store;
+
+import org.mule.runtime.api.i18n.I18nMessage;
+
+/**
+ * This exception is thrown when the underlying to an {@link ObjectStore}'s system fails.
+ *
+ * @since 1.0
+ */
+public class ObjectStoreNotAvailableException extends ObjectStoreException {
+
+  public ObjectStoreNotAvailableException() {
+    super();
+  }
+
+  public ObjectStoreNotAvailableException(I18nMessage message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ObjectStoreNotAvailableException(I18nMessage message) {
+    super(message);
+  }
+
+  public ObjectStoreNotAvailableException(Throwable cause) {
+    super(cause);
+  }
+}
+
+
