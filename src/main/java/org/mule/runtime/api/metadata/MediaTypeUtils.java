eeabc3bfc42e6c8ab48c0222799673d65dc81d0a
Esteban Wasinger <estebanwasinger@gmail.com>
2017-06-08 17:54:16 -0300

    MULE-12243: JMS should publish messages with String Representable Mim… (#240)
    * MULE-12243: JMS should publish messages with String Representable MimeTypes as StringMessages
    
    * MULE-12243: JMS should publish messages with String Representable MimeTypes as StringMessages
    
    * MULE-12243: JMS should publish messages with String Representable MimeTypes as StringMessages
    
    * MULE-12243: JMS should publish messages with String Representable MimeTypes as StringMessages
    
    * MULE-12243: JMS should publish messages with String Representable MimeTypes as StringMessages
    
    * MULE-12243: JMS should publish messages with String Representable MimeTypes as StringMessages

---
+++ src/main/java/org/mule/runtime/api/metadata/MediaTypeUtils.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.metadata;
+
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
+import static org.mule.runtime.api.util.Preconditions.checkNotNull;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Utility class to share logic about {@link MediaType media types}
+ *
+ * @since 1.0
+ * @see MediaType
+ */
+public final class MediaTypeUtils {
+
+  private static final String TEXT = "text";
+  private static final List<MediaType> STRING_REPRESENTABLE_MIME_TYPES = unmodifiableList(Stream.of(
+                                                                                                    "application/json",
+                                                                                                    "application/xml",
+                                                                                                    "application/javascript",
+                                                                                                    "application/rtf",
+                                                                                                    "application/csv",
+                                                                                                    "application/vnd.mozilla.xul+xml",
+                                                                                                    "application/x-csh",
+                                                                                                    "application/x-sh",
+                                                                                                    "application/xhtml+xml",
+                                                                                                    "image/svg+xml",
+                                                                                                    "text/*")
+      .map(MediaType::parse)
+      .collect(toList()));
+
+
+  private MediaTypeUtils() {}
+
+  /**
+   * @return the {@link List} of {@link MediaType} that are considered as {@link String} representable
+   */
+  public static List<MediaType> getStringRepresentableMimeTypes() {
+    return STRING_REPRESENTABLE_MIME_TYPES;
+  }
+
+  /**
+   * Verifies that the given {@code mediaType} is representable as a {@link String}
+   *
+   * @param mediaType to verify
+   * @return boolean indicating whether the {@link MediaType} is {@link String} representable or not
+   */
+  public static boolean isStringRepresentable(MediaType mediaType) {
+    checkNotNull(mediaType, "'mediaType' parameter can not be null");
+    return mediaType.getPrimaryType().equals(TEXT) || STRING_REPRESENTABLE_MIME_TYPES
+        .stream()
+        .anyMatch(type -> type.matches(mediaType));
+  }
+}
