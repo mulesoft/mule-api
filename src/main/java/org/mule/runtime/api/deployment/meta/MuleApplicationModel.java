9c3582cb11a2b225cd2bba68e6c9f8f8d20dbdb8
Pablo Kraan <pablokraan@users.noreply.github.com>
2017-06-19 18:23:18 +0200

    MULE-9653: Add a way to share application plugins on the domain (#248)
    _ Adding domain model
    _ Refactoring

--- src/main/java/org/mule/runtime/api/deployment/meta/MuleApplicationModel.java
+++ src/main/java/org/mule/runtime/api/deployment/meta/MuleApplicationModel.java
@@ -6,13 +6,8 @@
  */
 package org.mule.runtime.api.deployment.meta;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.unmodifiableList;
 import static java.util.Optional.ofNullable;
-import static org.apache.commons.lang3.StringUtils.isBlank;
-import static org.mule.runtime.api.util.Preconditions.checkArgument;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,28 +17,18 @@ import java.util.Optional;
  *
  * @since 1.0
  */
-public class MuleApplicationModel extends AbstractMuleArtifactModel {
+public class MuleApplicationModel extends MuleDeployableModel {
 
-  private final List<String> configs;
   private final String domain;
-  private final boolean redeploymentEnabled;
 
   private MuleApplicationModel(String name, String minMuleVersion,
                                MuleArtifactLoaderDescriptor classLoaderModelLoaderDescriptor,
                                MuleArtifactLoaderDescriptor bundleDescriptor, List<String> configs,
                                Optional<String> domain, Optional<Boolean> redeploymentEnabled) {
-    super(name, minMuleVersion, classLoaderModelLoaderDescriptor, bundleDescriptor);
-    this.configs = configs == null ? new ArrayList<>() : configs;
+    super(name, minMuleVersion, classLoaderModelLoaderDescriptor, bundleDescriptor, configs, redeploymentEnabled);
     this.domain = domain.orElse(null);
-    this.redeploymentEnabled = redeploymentEnabled.orElse(true);
   }
 
-  /**
-   * @return the application configuration files
-   */
-  public List<String> getConfigs() {
-    return configs == null ? emptyList() : unmodifiableList(configs);
-  }
 
   /**
    * @return the domain associated with this application
@@ -53,45 +38,18 @@ public class MuleApplicationModel extends AbstractMuleArtifactModel {
   }
 
   /**
-   * @return true if the application supports redeployment by changing a configuration file.
-   */
-  public boolean isRedeploymentEnabled() {
-    return redeploymentEnabled;
-  }
-
-  /**
    * A builder to create instances of {@link MuleApplicationModel}.
    *
    * @since 1.0
    */
   public static class MuleApplicationModelBuilder
-      extends AbstractMuleArtifactModelBuilder<MuleApplicationModelBuilder, MuleApplicationModel> {
+      extends MuleDeployableModelBuilder<MuleApplicationModelBuilder, MuleApplicationModel> {
 
-    private List<String> configs = new ArrayList<>();
     private String domain;
-    private Boolean redeploymentEnabled;
-
-    /**
-     * @param configs the set of application configuration files
-     */
-    public void setConfigs(List<String> configs) {
-      this.configs = configs;
-    }
 
     @Override
     protected MuleApplicationModelBuilder getThis() {
       return this;
-    }
-
-    /**
-     * @return a well formed {@link MuleApplicationModel}
-     */
-    public MuleApplicationModel build() {
-      checkArgument(!isBlank(getName()), "name cannot be a blank");
-      checkArgument(getMinMuleVersion() != null, "minMuleVersion cannot be null");
-      checkArgument(getBundleDescriptorLoader() != null, "bundleDescriber cannot be null");
-      return new MuleApplicationModel(getName(), getMinMuleVersion(), getClassLoaderModelDescriptorLoader(),
-                                      getBundleDescriptorLoader(), configs, ofNullable(domain), ofNullable(redeploymentEnabled));
     }
 
     /**
@@ -103,11 +61,11 @@ public class MuleApplicationModel extends AbstractMuleArtifactModel {
       return this;
     }
 
-    /**
-     * @param redeploymentEnabled true if the application supports redeployment, false otherwise.
-     */
-    public void setRedeploymentEnabled(boolean redeploymentEnabled) {
-      this.redeploymentEnabled = redeploymentEnabled;
+    @Override
+    protected MuleApplicationModel doCreateModel(List<String> configs, Boolean redeploymentEnabled) {
+      return new MuleApplicationModel(getName(), getMinMuleVersion(), getClassLoaderModelDescriptorLoader(),
+                                      getBundleDescriptorLoader(), configs, ofNullable(domain), ofNullable(redeploymentEnabled));
+
     }
   }
 }
---
+++ src/main/java/org/mule/runtime/api/deployment/persistence/MuleDomainModelJsonSerializer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.deployment.persistence;
+
+import org.mule.runtime.api.deployment.meta.MuleApplicationModel;
+import org.mule.runtime.api.deployment.meta.MuleDomainModel;
+
+/**
+ * {@link MuleApplicationModel} json serializer.
+ *
+ * @since 1.0
+ */
+public class MuleDomainModelJsonSerializer extends AbstractMuleArtifactModelJsonSerializer<MuleDomainModel> {
+
+  @Override
+  protected Class<MuleDomainModel> getParameterizedClass() {
+    return MuleDomainModel.class;
+  }
+
+}
