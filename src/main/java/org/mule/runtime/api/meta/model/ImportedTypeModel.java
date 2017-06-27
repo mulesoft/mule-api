9d859c937148f24e24669cd71f0bb58f54884554
Pablo <pablomusumeci@users.noreply.github.com>
2017-06-27 14:18:16 -0300

    MULE-11739: Make ImportedTypes not dependent on Extension name (#254)

--- src/main/java/org/mule/runtime/api/meta/model/ImportedTypeModel.java
+++ src/main/java/org/mule/runtime/api/meta/model/ImportedTypeModel.java
@@ -8,47 +8,31 @@ package org.mule.runtime.api.meta.model;
 
 
 import static java.util.Objects.hash;
-import org.mule.metadata.api.model.MetadataType;
-
-import org.apache.commons.lang3.StringUtils;
+import static org.mule.runtime.api.util.Preconditions.checkArgument;
+import org.mule.metadata.api.model.ObjectType;
 
 /**
  * A model which describes that an extension is importing an
- * {@link #getImportedType() imported type} from an extension of
- * a given {@link #getOriginExtensionName() name}
+ * {@link #getImportedType() imported type}.
  *
  * @since 1.0
  */
 public final class ImportedTypeModel {
 
-  private final String originExtensionName;
-  private final MetadataType importedType;
+  private final ObjectType importedType;
 
   /**
    * Creates a new instance
    *
-   * @param originExtensionName the name of the extension which originally defines the type
    * @param importedType        the type to be imported
    * @throws IllegalArgumentException if {@code originExtensionName} is blank or {@code importedType} is {@code null}
    */
-  public ImportedTypeModel(String originExtensionName, MetadataType importedType) {
-    if (StringUtils.isBlank(originExtensionName)) {
-      throw new IllegalArgumentException("originExtensionName cannot be blank");
-    }
-
-    if (importedType == null) {
-      throw new IllegalArgumentException("importedType cannot be null");
-    }
-
-    this.originExtensionName = originExtensionName;
+  public ImportedTypeModel(ObjectType importedType) {
+    checkArgument(importedType != null, "importedType cannot be null");
     this.importedType = importedType;
   }
 
-  public String getOriginExtensionName() {
-    return originExtensionName;
-  }
-
-  public MetadataType getImportedType() {
+  public ObjectType getImportedType() {
     return importedType;
   }
 
@@ -56,7 +40,7 @@ public final class ImportedTypeModel {
   public boolean equals(Object obj) {
     if (obj instanceof ImportedTypeModel) {
       ImportedTypeModel other = (ImportedTypeModel) obj;
-      return originExtensionName.equals(other.getOriginExtensionName()) && importedType.equals(other.getImportedType());
+      return importedType.equals(other.getImportedType());
     }
 
     return false;
@@ -64,6 +48,6 @@ public final class ImportedTypeModel {
 
   @Override
   public int hashCode() {
-    return hash(importedType, originExtensionName);
+    return hash(importedType);
   }
 }
