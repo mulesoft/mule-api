10c027895d6d0bc723172e558c4a80434c08ce8f
alegmarra <alemarra@gmail.com>
2017-07-03 19:23:07 -0300

    MULE-11599: Support for defining DW functions

--- src/main/java/org/mule/runtime/api/meta/model/config/ConfigurationModel.java
+++ src/main/java/org/mule/runtime/api/meta/model/config/ConfigurationModel.java
@@ -14,6 +14,7 @@ import org.mule.runtime.api.meta.model.HasExternalLibraries;
 import org.mule.runtime.api.meta.model.display.HasDisplayModel;
 import org.mule.runtime.api.meta.model.connection.ConnectionProviderModel;
 import org.mule.runtime.api.meta.model.connection.HasConnectionProviderModels;
+import org.mule.runtime.api.meta.model.function.HasFunctionModels;
 import org.mule.runtime.api.meta.model.operation.HasOperationModels;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
@@ -40,6 +41,6 @@ import org.mule.runtime.api.meta.model.source.SourceModel;
  * @since 1.0
  */
 public interface ConfigurationModel extends NamedObject, DescribedObject, EnrichableModel, ParameterizedModel,
-    HasOperationModels, HasSourceModels, HasConnectionProviderModels, HasDisplayModel, HasExternalLibraries {
+    HasOperationModels, HasSourceModels, HasConnectionProviderModels, HasDisplayModel, HasExternalLibraries, HasFunctionModels {
 
 }
