489abbd265e673c0c75a3be4ff49e585e7d0166e
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-07-03 11:45:52 -0300

    MULE-11364: Utility methods (#262)

--- src/main/java/org/mule/runtime/api/metadata/TypedValue.java
+++ src/main/java/org/mule/runtime/api/metadata/TypedValue.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 
 /**
  * Maintains a value that has an associated {@link DataType}.
- * 
+ *
  * @param <T> the content type.
  * @since 1.0
  */
@@ -20,13 +20,49 @@ public final class TypedValue<T> implements Serializable {
 
   private static final long serialVersionUID = -2533879516750283994L;
 
+  /**
+   * Utility method to obtain a type value's content, in cases in which you don't know if
+   * the {@ccode value} is a type value at all.
+   * <p>
+   * If {@code value} is a TypeValue, then {@link #getValue()} is returned. Otherwise,
+   * the {@code value} is returned as is (even if it's {@code null})
+   *
+   * @param value a value which may or may not be a TypeValue
+   * @param <T>   the output's generic type
+   * @return an unwrapped value
+   */
+  public static <T> T unwrap(Object value) {
+    if (value instanceof TypedValue) {
+      return ((TypedValue<T>) value).getValue();
+    }
+
+    return (T) value;
+  }
+
+  /**
+   * Creates a new instance for the given {@code value}, using an auto calculated
+   * {@link DataType}. This method is useful when no particular media types or
+   * encoding are required
+   *
+   * @param value this object's content
+   * @param <T>   the value's generic type
+   * @return a new {@link TypedValue}
+   */
+  public static <T> TypedValue<T> of(T value) {
+    if (value instanceof TypedValue) {
+      return (TypedValue<T>) value;
+    }
+
+    return new TypedValue<>(value, DataType.fromObject(value));
+  }
+
   private final T value;
   private final DataType dataType;
 
   /**
    * Constructs a new {@link TypedValue} with the given parameters.
-   * 
-   * @param value this object's content.
+   *
+   * @param value    this object's content.
    * @param dataType the {@link DataType} for this object's content.
    */
   public TypedValue(T value, DataType dataType) {
