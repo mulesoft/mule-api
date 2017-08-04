57a9bed8beaf79ce4c8c13cc090fee897a787b97
Ana Felisatti <afelisatti@mulesoft.com>
2017-08-04 13:29:28 -0300

    MULE-10421: Define and implement how to deal with composite exceptions (#298)

---
+++ src/main/java/org/mule/runtime/api/exception/ComposedErrorException.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.exception;
+
+import org.mule.runtime.api.message.Error;
+
+import java.util.List;
+
+/**
+ * Exception that holds a collection of child {@link Error Errors}.
+ * When this exception is analysed to create an {@link org.mule.runtime.api.message.Error}, it will feature the collection.
+ *
+ * @since 1.0
+ */
+public interface ComposedErrorException {
+
+  /**
+   * Provides the list of errors associated to this exception.
+   *
+   * @return a list of child errors
+   */
+  List<Error> getErrors();
+
+}
