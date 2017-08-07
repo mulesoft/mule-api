3dda69d6c6cd8441f35e0b75c206419571718da2
pablolagreca <pablolagreca@hotmail.com>
2017-08-07 21:33:30 +0200

    Mule 13282 (#300)
    * MULE-13282 - Add a mechanism from the API to access MetadataService, ConnectivityService and ValueProviderService
    
    * missing changes

---
+++ src/main/java/org/mule/runtime/api/connectivity/ConnectivityTestingService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.connectivity;
+
+import org.mule.runtime.api.component.location.Location;
+import org.mule.runtime.api.connection.ConnectionValidationResult;
+import org.mule.runtime.api.exception.ObjectNotFoundException;
+
+/**
+ * Service for doing connectivity testing.
+ *
+ * A {@code ConnectivityTestingService}
+ *
+ * @since 4.0
+ */
+public interface ConnectivityTestingService {
+
+  /**
+   * Key under which the {@link ConnectivityTestingService} can be found in the {@link org.mule.runtime.api.artifact.Registry}
+   */
+  String CONNECTIVITY_TESTING_SERVICE_KEY = "_muleConnectivityTestingService";
+
+  /**
+   * Does connection testing over the component registered under the provider identifier
+   *
+   * @param location component identifier over the one connectivity testing is done.
+   * @return connectivity testing result.
+   * @throws UnsupportedConnectivityTestingObjectException when it's not possible to do connectivity testing over the mule
+   *         component.
+   * @throws {@link ObjectNotFoundException} when the object to use to do connectivity does
+   *         not exists
+   */
+  ConnectionValidationResult testConnection(Location location);
+
+}
