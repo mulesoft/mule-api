e90e9774e54875c6e043b93e3aeb2d2022096abf
pablolagreca <Neruda10>
2017-08-16 20:51:17 -0300

    more changes

---
+++ src/main/java/org/mule/runtime/api/component/execution/ComponentExecutionException.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.component.execution;
+
+import org.mule.runtime.api.event.Event;
+import org.mule.runtime.api.exception.MuleRuntimeException;
+
+/**
+ * Exception thrown by {@link ExecutableComponent} when there was a unhandled error within the execution of a set of components.
+ * 
+ * @since 1.0
+ */
+public class ComponentExecutionException extends MuleRuntimeException {
+
+  private final Event event;
+
+  /**
+   * @param cause the exception thrown by the failing component.
+   * @param event the {@link Event} at the moment of the exception.
+   */
+  public ComponentExecutionException(Throwable cause, Event event) {
+    super(cause);
+    this.event = event;
+  }
+
+  /**
+   * @return the {@link Event} at the moment of the exception.
+   */
+  public Event getEvent() {
+    return event;
+  }
+}
--- src/main/java/org/mule/runtime/api/component/execution/ExecutableComponent.java
+++ src/main/java/org/mule/runtime/api/component/execution/ExecutableComponent.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.api.component;
+package org.mule.runtime.api.component.execution;
 
 import org.mule.runtime.api.event.Event;
 import org.mule.runtime.api.event.InputEvent;
