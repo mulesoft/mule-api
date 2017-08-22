499e934c773c06aec3088089bd04dbd66b2dce59
Rodrigo <elrodro83@users.noreply.github.com>
2017-08-22 10:03:16 -0300

    MULE-13263: Interception API no calling before & around if the processor (#315)

---
+++ src/main/java/org/mule/runtime/api/interception/ProcessorParameterValue.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.interception;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
+
+/**
+ * Contains information about a single parameter of an intercepted component.
+ *
+ * @since 1.0
+ */
+public interface ProcessorParameterValue {
+
+  /**
+   * @return the name of the parameter, as defined in the DSL.
+   */
+  String parameterName();
+
+  /**
+   * Allows to query the unresolved value originally configured in the application.
+   *
+   * @return the value as a {@link String}, as it is configured in the application configuration, or {@code null} for implicit
+   *         operation parameters.
+   */
+  String providedValue();
+
+  /**
+   * Performs any required expression resolution on {@link #providedValue()}.
+   *
+   * @return if {@link #providedValue()} was an expression, its resolved value. Otherwise, the same as {@link #providedValue()}.
+   * @throws MuleRuntimeException if an error occurs while resolving the value.
+   */
+  Object resolveValue();
+
+}
