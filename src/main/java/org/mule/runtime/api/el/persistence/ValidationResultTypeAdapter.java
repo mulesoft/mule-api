39fd33679c200d5824aeccde8bf926a5c8df4c71
Martin Buchwald <martin.buchwald@mulesoft.com>
2017-09-28 11:31:56 -0300

    ValidationResult Serializer

---
+++ src/main/java/org/mule/runtime/api/el/persistence/ValidationResultTypeAdapter.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.el.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import org.mule.runtime.api.el.ValidationResult;
+import org.mule.runtime.api.el.validation.ValidationMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationResultTypeAdapter extends TypeAdapter<ValidationResult> {
+
+  private Gson gson = new GsonBuilder().create();
+
+  @Override
+  public void write(JsonWriter jsonWriter, ValidationResult validationResult) throws IOException {
+    gson.toJson(validationResult, ValidationResult.class, jsonWriter);
+  }
+
+  @Override
+  public ValidationResult read(JsonReader jsonReader) throws IOException {
+    JsonElement jsonElement = new JsonParser().parse(jsonReader);
+    if (jsonElement.isJsonNull()) {
+      return null;
+    }
+    JsonObject validationResult = jsonElement.getAsJsonObject();
+
+    boolean isSuccess = validationResult.get("result").getAsBoolean();
+    if (isSuccess) {
+      return ValidationResult.success();
+    }
+
+    String errorMessage = validationResult.get("errorMessage").getAsString();
+    List<ValidationMessage> messages = new ArrayList<>();
+    validationResult.get("messages").getAsJsonArray()
+        .forEach(validationMessage -> messages.add(gson.fromJson(validationMessage, ValidationMessage.class)));
+
+    return ValidationResult.failure(errorMessage, messages);
+  }
+
+}
