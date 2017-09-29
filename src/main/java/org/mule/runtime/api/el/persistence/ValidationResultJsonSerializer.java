93f2141085654c6871955a84695b09d809d1faa6
mbuchwald <martin.buchwald@mulesoft.com>
2017-09-29 12:12:25 -0300

    MULE-13596: DW validation/preview should support functions (#371)

---
+++ src/main/java/org/mule/runtime/api/el/persistence/ValidationResultJsonSerializer.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.el.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.mule.runtime.api.el.ValidationResult;
+
+public class ValidationResultJsonSerializer {
+
+  private Gson gson;
+
+  public ValidationResultJsonSerializer() {
+    gson = new GsonBuilder().registerTypeAdapterFactory(new ValidationResultTypeAdapterFactory()).create();
+  }
+
+  public String serialize(ValidationResult validationResult) {
+    return this.gson.toJson(validationResult);
+  }
+
+  public ValidationResult deserialize(String validationResult) {
+    return this.gson.fromJson(validationResult, ValidationResult.class);
+  }
+
+}
---
+++ src/main/java/org/mule/runtime/api/el/persistence/ValidationResultTypeAdapterFactory.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.el.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import org.mule.runtime.api.el.ValidationResult;
+
+/**
+ * {@link TypeAdapterFactory} implementation for creating {@link ValidationResult} {@link TypeAdapter}s
+ *
+ * @since 1.0
+ */
+public class ValidationResultTypeAdapterFactory implements TypeAdapterFactory {
+
+  @Override
+  public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken) {
+    if (ValidationResult.class.isAssignableFrom(typeToken.getRawType())) {
+      return (TypeAdapter<T>) new ValidationResultTypeAdapter(gson);
+    }
+    return null;
+  }
+}
