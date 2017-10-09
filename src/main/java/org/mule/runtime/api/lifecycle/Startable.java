aa32bad3ac5f93ee3105c46c842d44d221b4a7dd
pablolagreca <pablolagreca@hotmail.com>
2017-10-09 15:51:31 -0300

    MULE-13531 - Inconsistent lifecycle on ObjectFactory instances (#385)

--- src/main/java/org/mule/runtime/api/lifecycle/Startable.java
+++ src/main/java/org/mule/runtime/api/lifecycle/Startable.java
@@ -9,14 +9,16 @@ package org.mule.runtime.api.lifecycle;
 import org.mule.runtime.api.exception.MuleException;
 
 /**
- * {@code Startable} provides an object with a {@link #start()} method which gets called when the Mule instance gets started.
- * This is mostly used by infrastructure components, but can also be implemented by service objects.
- *
- * This lifecycle interface should always be implemented with its opposite lifecycle interface
- * {@link Stoppable}.
- *
+ * {@code Startable} provides an object with a {@link #start()} method which gets called when the Mule instance gets started. This
+ * is mostly used by infrastructure components, but can also be implemented by service objects.
+ * <p/>
+ * This lifecycle interface should always be implemented with its opposite lifecycle interface {@link Stoppable}.
+ * <p/>
+ * In case that the {@link #start()} method execution fails then mule will call the {@link Stoppable#stop()} method if the class
+ * also implements {@link Stoppable} allowing the object to dispose any allocated resource during {@link #start()}
+ * <p/>
+ * 
  * @see Stoppable
- *
  */
 public interface Startable {
 
