8f314f077f0f3c48f25f7161ad712348fc4f95bb
pablolagreca <pablolagreca@hotmail.com>
2017-10-12 11:48:06 -0300

    MULE-13776 - Expose scheduler strategies configuration in API (#388)

---
+++ src/main/java/org/mule/runtime/api/source/CronSchedulerConfiguration.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.source;
+
+/**
+ * Configuration interface for cron scheduler
+ *
+ * @since 1.0
+ */
+public interface CronSchedulerConfiguration extends SchedulerConfiguration {
+
+  /**
+   * @return the cron expression
+   */
+  String getExpression();
+
+  /**
+   * @return The ID of the time zone in which the expression will be based. Refer to {@code java.util.TimeZone} for the format and
+   *         possible values of the timeZone ID.
+   */
+  String getTimeZone();
+
+}
