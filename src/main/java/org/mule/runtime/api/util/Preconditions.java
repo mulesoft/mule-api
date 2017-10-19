4b779b26c48531579e187c1347c404e15b083bde
Daniel Feist <dfeist@gmail.com>
2017-10-19 14:08:06 +0100

    Review

--- src/main/java/org/mule/runtime/api/util/Preconditions.java
+++ src/main/java/org/mule/runtime/api/util/Preconditions.java
@@ -64,25 +64,11 @@ public class Preconditions {
    * @param errorMessage the exception message to use if the check fails
    * @return the non-null reference that was validated
    * @throws NullPointerException if {@code reference} is null
+   * @deprecated Use {@link java.util.Objects#requireNonNull(Object, String)}
    */
   public static <T> T checkNotNull(T reference, String errorMessage) {
     if (reference == null) {
       throw new NullPointerException(errorMessage);
-    }
-    return reference;
-  }
-
-  /**
-   * Ensures that an object reference passed as a parameter to the calling method is not null.
-   *
-   * @param reference an object reference
-   * @param errorMessage the exception message supplier to use if the check fails
-   * @return the non-null reference that was validated
-   * @throws NullPointerException if {@code reference} is null
-   */
-  public static <T> T checkNotNull(T reference, Supplier<String> errorMessage) {
-    if (reference == null) {
-      throw new NullPointerException(errorMessage.get());
     }
     return reference;
   }
