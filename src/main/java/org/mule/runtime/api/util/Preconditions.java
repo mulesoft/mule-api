b83afdef4b4040eff586cd0d308fecadd5c740d0
Daniel Feist <dfeist@gmail.com>
2017-10-19 14:21:05 +0100

    MULE-13824 Add ability to evaluate error message lazily when using pr… (#393)

--- src/main/java/org/mule/runtime/api/util/Preconditions.java
+++ src/main/java/org/mule/runtime/api/util/Preconditions.java
@@ -7,6 +7,8 @@
 
 package org.mule.runtime.api.util;
 
+import java.util.function.Supplier;
+
 /**
  * <p>
  * Utility class to validate Preconditions
@@ -25,6 +27,16 @@ public class Preconditions {
   }
 
   /**
+   * @param condition Condition that the argument must satisfy
+   * @param message The Message supplier for the exception in case the condition is invalid
+   */
+  public static void checkArgument(boolean condition, Supplier<String> message) {
+    if (!condition) {
+      throw new IllegalArgumentException(message.get());
+    }
+  }
+
+  /**
    * @param condition Condition that must be satisfied
    * @param message The Message of the exception in case the condition is invalid
    */
@@ -35,13 +47,26 @@ public class Preconditions {
   }
 
   /**
+   * @param condition Condition that must be satisfied
+   * @param message The Message of the exception in case the condition is invalid
+   * @param message The Message supplier for the exception in case the condition is invalid
+   */
+  public static void checkState(boolean condition, Supplier<String> message) {
+    if (!condition) {
+      throw new IllegalStateException(message.get());
+    }
+  }
+
+  /**
    * Ensures that an object reference passed as a parameter to the calling method is not null.
    *
    * @param reference an object reference
    * @param errorMessage the exception message to use if the check fails
    * @return the non-null reference that was validated
    * @throws NullPointerException if {@code reference} is null
+   * @deprecated Use {@link java.util.Objects#requireNonNull(Object, String)}
    */
+  @Deprecated
   public static <T> T checkNotNull(T reference, String errorMessage) {
     if (reference == null) {
       throw new NullPointerException(errorMessage);
