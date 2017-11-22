eaa5602895a186d2d1e83405a842339683ec73bb
Alejandro Pulver <alejandro.pulver@mulesoft.com>
2017-11-22 10:25:01 -0300

    MULE-13910: differentiate source and flow overload conditions

---
+++ src/main/java/org/mule/runtime/api/exception/FlowOverloadException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.exception;
+
+import java.util.concurrent.RejectedExecutionException;
+
+public class FlowOverloadException extends RejectedExecutionException {
+
+  private static final long serialVersionUID = -1456137134254209436L;
+
+  /**
+   * Constructs a new exception with the specified message.
+   *
+   * @param message the detail message
+   */
+  public FlowOverloadException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new exception with the specified cause.
+   *
+   * @param cause the exception cause
+   */
+  public FlowOverloadException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs a new exception with the specified message and cause.
+   *
+   * @param message the detail message
+   * @param cause the exception cause
+   */
+  public FlowOverloadException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/org/mule/runtime/api/exception/SourceOverloadException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.exception;
+
+import java.util.concurrent.RejectedExecutionException;
+
+public class SourceOverloadException extends RejectedExecutionException {
+
+  private static final long serialVersionUID = 2992038225993918910L;
+
+  /**
+   * Constructs a new exception with the specified message.
+   *
+   * @param message the detail message
+   */
+  public SourceOverloadException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new exception with the specified cause.
+   *
+   * @param cause the exception cause
+   */
+  public SourceOverloadException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs a new exception with the specified message and cause.
+   *
+   * @param message the detail message
+   * @param cause the exception cause
+   */
+  public SourceOverloadException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
