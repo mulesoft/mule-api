e97b4ade61f1706112105e7901661dee6706fced
JuanDesi <juan.desimoni@mulesoft.com>
2017-11-29 14:13:13 -0300

    MULE-14038: Support static custom types

---
+++ src/main/java/org/mule/runtime/api/metadata/resolving/InputStaticTypeResolver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.metadata.resolving;
+
+import org.mule.metadata.api.model.MetadataType;
+import org.mule.runtime.api.meta.model.parameter.ParameterModel;
+import org.mule.runtime.api.metadata.MetadataContext;
+
+/**
+ * {@link StaticResolver} implementation for the {@link ParameterModel}s.
+ * <p>
+ * This {@link InputTypeResolver} is discarded once the extension model is built. It's not registered with the dynamic
+ * {@link InputTypeResolver}s.
+ *
+ * @since 1.1
+ */
+public abstract class InputStaticTypeResolver implements InputTypeResolver, StaticResolver {
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * The category name is not used for {@link StaticResolver}s since they are not registered in the extension.
+   */
+  @Override
+  public String getCategoryName() {
+    return "INPUT_" + STATIC_RESOLVER_NAME;
+  }
+
+  /**
+   * This method resolves the static output type for a {@link ParameterModel} proxing to the
+   * {@link StaticResolver#getStaticMetadata()} method.
+   * <p>
+   * The context and key are not used nor injected.
+   */
+  @Override
+  final public MetadataType getInputMetadata(MetadataContext context, Object key) {
+    return getStaticMetadata();
+  }
+}
---
+++ src/main/java/org/mule/runtime/api/metadata/resolving/StaticResolver.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.metadata.resolving;
+
+import org.mule.metadata.api.model.MetadataType;
+import org.mule.runtime.api.connection.ConnectionException;
+import org.mule.runtime.api.meta.Typed;
+import org.mule.runtime.api.metadata.MetadataContext;
+import org.mule.runtime.api.metadata.MetadataResolvingException;
+
+/**
+ * Handles the construction of a custom {@link MetadataType} in any format to be set in a {@link Typed} component statically
+ * when building the extension without going through the metadata service.
+ * <p>
+ * Provided implementations of this interfaces should be used and not this one directly.
+ *
+ * @since 1.1
+ */
+public interface StaticResolver extends NamedTypeResolver {
+
+  String STATIC_RESOLVER_NAME = "STATIC_RESOLVER";
+
+  /**
+   * Creates a new {@link MetadataType}
+   *
+   * @return a new {@link MetadataType} to be set in a {@link Typed} component.
+   */
+  MetadataType getStaticMetadata();
+}
