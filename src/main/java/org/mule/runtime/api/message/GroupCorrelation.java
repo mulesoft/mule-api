0e3bc24fe8edc76ef4f40b17f4685cbb8daefb8b
luciano.raineri <luciano.raineri@mulesoft.com>
2018-01-04 13:30:39 -0300

    changed javadoc

--- src/main/java/org/mule/runtime/api/message/GroupCorrelation.java
+++ src/main/java/org/mule/runtime/api/message/GroupCorrelation.java
@@ -14,7 +14,7 @@ import java.util.OptionalInt;
 /**
  * Immutable container for correlation properties relative to a {@link Message}.
  * 
- * @since 4.1
+ * @since 1.1
  */
 public class GroupCorrelation implements Serializable {
 
