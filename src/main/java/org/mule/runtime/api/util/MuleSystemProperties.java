828d64db7296b7e83f65b342e14d92152439ef0c
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-08-24 13:30:17 -0300

    MULE-15590: Model validators should only be executed when packaging (#501)

---
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.util;
+
+import static java.lang.System.getProperty;
+
+/**
+ * Defines Mule System properties.
+ *
+ * @since 1.2.0
+ */
+public final class MuleSystemProperties {
+
+  public static final String SYSTEM_PROPERTY_PREFIX = "mule.";
+
+  public static final String MULE_CONTEXT_PROPERTY = SYSTEM_PROPERTY_PREFIX + "context";
+  public static final String MULE_ENCODING_SYSTEM_PROPERTY = SYSTEM_PROPERTY_PREFIX + "encoding";
+  public static final String MULE_SECURITY_SYSTEM_PROPERTY = SYSTEM_PROPERTY_PREFIX + "security.model";
+  public static final String MULE_SECURITY_PROVIDER_PROPERTY = SYSTEM_PROPERTY_PREFIX + "security.provider";
+  public static final String MULE_STREAMING_BUFFER_SIZE = SYSTEM_PROPERTY_PREFIX + "streaming.bufferSize";
+
+  /**
+   * System property key for the default size of a streaming buffer bucket
+   */
+  public static final String MULE_STREAMING_BUCKET_SIZE = SYSTEM_PROPERTY_PREFIX + "streaming.bucketSize";
+  public static final String TESTING_MODE_PROPERTY_NAME = SYSTEM_PROPERTY_PREFIX + "testingMode";
+
+  /**
+   * Forces the validation of all loaded extension models
+   */
+  public static final String FORCE_EXTENSION_VALIDATION_PROPERTY_NAME = SYSTEM_PROPERTY_PREFIX + "forceExtensionValidation";
+
+  public static final String MULE_STREAMING_MAX_MEMORY = SYSTEM_PROPERTY_PREFIX + "max.streaming.memory";
+  public static final String MULE_SIMPLE_LOG = SYSTEM_PROPERTY_PREFIX + "simpleLog";
+  public static final String MULE_FORCE_CONSOLE_LOG = SYSTEM_PROPERTY_PREFIX + "forceConsoleLog";
+  public static final String MULE_LOG_CONTEXT_DISPOSE_DELAY_MILLIS = SYSTEM_PROPERTY_PREFIX + "log.context.dispose.delay.millis";
+  public static final String MULE_LOG_DEFAULT_POLICY_INTERVAL =
+      SYSTEM_PROPERTY_PREFIX + "log.defaultAppender.timeBasedTriggerPolicy.interval";
+  public static final String MULE_LOG_DEFAULT_STRATEGY_MAX = SYSTEM_PROPERTY_PREFIX + "log.defaultAppender.rolloverStrategy.max";
+  public static final String MULE_LOG_DEFAULT_STRATEGY_MIN = SYSTEM_PROPERTY_PREFIX + "log.defaultAppender.rolloverStrategy.min";
+  public static final String MULE_FLOW_TRACE = SYSTEM_PROPERTY_PREFIX + "flowTrace";
+  public static final String MULE_LOG_VERBOSE_CLASSLOADING = SYSTEM_PROPERTY_PREFIX + "classloading.verbose";
+  public static final String MULE_MEL_AS_DEFAULT = SYSTEM_PROPERTY_PREFIX + "test.mel.default";
+  public static final String MULE_DISABLE_RESPONSE_TIMEOUT = SYSTEM_PROPERTY_PREFIX + "timeout.disable";
+  public static final String MULE_ALLOW_JRE_EXTENSION = SYSTEM_PROPERTY_PREFIX + "classloading.jreExtension";
+  public static final String MULE_JRE_EXTENSION_PACKAGES = SYSTEM_PROPERTY_PREFIX + "classloading.jreExtension.packages";
+  public static final String MULE_LOGGING_INTERVAL_SCHEDULERS_LATENCY_REPORT =
+      SYSTEM_PROPERTY_PREFIX + "schedulers.latency.report.interval";
+
+  private static final boolean TESTING_MODE = getProperty(TESTING_MODE_PROPERTY_NAME) != null;
+  private static final boolean FORCE_EXTENSION_VALIDATION = getProperty(FORCE_EXTENSION_VALIDATION_PROPERTY_NAME) != null;
+
+  /**
+   * @return {@code true} if the {@link #TESTING_MODE_PROPERTY_NAME} property has been set (regardless of the value)
+   */
+  public static boolean isTestingMode() {
+    return TESTING_MODE;
+  }
+
+  /**
+   * @return {@code true} if the {@link #FORCE_EXTENSION_VALIDATION_PROPERTY_NAME} property has been set (regardless of the value)
+   */
+  public static boolean isForceExtensionValidation() {
+    return FORCE_EXTENSION_VALIDATION;
+  }
+
+  private MuleSystemProperties() {}
+}
