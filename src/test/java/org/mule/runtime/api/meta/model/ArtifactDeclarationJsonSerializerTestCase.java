2190bf9b563f7b2959c2167fe5392d001613d3a4
Pablo Kraan <pablokraan@users.noreply.github.com>
2018-08-24 15:50:46 -0300

    MULE-15599: Serializing an artifact model constructed from serializin… (#503)
    * MULE-15599: Serializing an artifact model constructed from serializing a JSON loses data
    
    _ Adding ignored test reproducing the issue
    _ Adding dependency to compare JSON data
    _ Formatting JSON test data
    
    * MULE-15599: Serializing an artifact model constructed from serializing a JSON loses data
    
    _ Renaming

--- src/test/java/org/mule/runtime/api/meta/model/ArtifactDeclarationJsonSerializerTestCase.java
+++ src/test/java/org/mule/runtime/api/meta/model/ArtifactDeclarationJsonSerializerTestCase.java
@@ -25,8 +25,11 @@ import com.google.gson.stream.JsonReader;
 
 import java.io.InputStreamReader;
 
+import org.json.JSONException;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class ArtifactDeclarationJsonSerializerTestCase {
 
@@ -55,6 +58,21 @@ public class ArtifactDeclarationJsonSerializerTestCase {
 
     ArtifactDeclaration artifactDeclaration = serializer.deserialize(json);
     assertThat(json, applicationDeclaration, is(equalTo(artifactDeclaration)));
+  }
+
+  @Test
+  @Ignore("See MULE-15599")
+  public void serializeDeserializeSerializeTest() throws JSONException {
+    ArtifactDeclarationJsonSerializer serializer = ArtifactDeclarationJsonSerializer.getDefault(true);
+    String originalJson = serializer.serialize(applicationDeclaration);
+
+    ArtifactDeclaration loadedDeclaration = serializer.deserialize(originalJson);
+
+    JsonParser parser = new JsonParser();
+    JsonElement jsonElement = parser.parse(ArtifactDeclarationJsonSerializer.getDefault(true).serialize(loadedDeclaration));
+    String generatedJson = jsonElement.toString();
+
+    JSONAssert.assertEquals(originalJson, generatedJson, true);
   }
 
   private ArtifactDeclaration createArtifact() {
