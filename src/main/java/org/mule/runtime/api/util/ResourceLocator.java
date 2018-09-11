51da00ed3550bb8a0bc93ce71f61625bbab6a374
Ana Felisatti <afelisatti@mulesoft.com>
2018-09-11 15:19:22 -0300

    MULE-15634: Evaluate classifier and type attributes usage (#514)

--- src/main/java/org/mule/runtime/api/util/ResourceLocator.java
+++ src/main/java/org/mule/runtime/api/util/ResourceLocator.java
@@ -39,8 +39,8 @@ public interface ResourceLocator {
   Optional<URL> find(String resource, Object caller);
 
   /**
-   * Looks for a resource as in {@link #findIn(String, String, String, Optional, Object)} and returns it's {@link InputStream} if
-   * possible.
+   * Same as {@link #loadFrom(String, String, String, Optional, String, String, Object)} but using no classifier and {@code jar} as
+   * type.
    *
    * @param resource the name of the resource to load
    * @param groupId the group ID of the artifact where to look
@@ -49,11 +49,46 @@ public interface ResourceLocator {
    * @param caller the calling object
    * @return a stream for this resource
    */
-  Optional<InputStream> loadFrom(String resource, String groupId, String artifactId, Optional<String> version, Object caller);
+  default Optional<InputStream> loadFrom(String resource, String groupId, String artifactId, Optional<String> version,
+                                         Object caller) {
+    return loadFrom(resource, groupId, artifactId, version, "", "jar", caller);
+  }
+
+  /**
+   * Looks for a resource as in {@link #findIn(String, String, String, Optional, String, String, Object)} and returns it's {@link InputStream} if
+   * possible.
+   *
+   * @param resource the name of the resource to load
+   * @param groupId the group ID of the artifact where to look
+   * @param artifactId the artifact ID of the artifact where to look
+   * @param version the optional version of the artifact where to look
+   * @param classifier the classifier of the artifact where to look
+   * @param type the type of the artifact where to look
+   * @param caller the calling object
+   * @return a stream for this resource
+   */
+  Optional<InputStream> loadFrom(String resource, String groupId, String artifactId, Optional<String> version, String classifier,
+                                 String type, Object caller);
 
   /**
    * Looks for a resource based on the calling class classloader and the current context classloader, specifically within the artifact
-   * specified by it's GAV coordinates.
+   * specified by it's GAV coordinates, considering it's classifier and type as well.
+   *
+   * @param resource the name of the resource to load
+   * @param groupId the group ID of the artifact where to look
+   * @param artifactId the artifact ID of the artifact where to look
+   * @param version the optional version of the artifact where to look
+   * @param classifier the classifier of the artifact where to look
+   * @param type the type of the artifact where to look
+   * @param caller the calling object
+   * @return the {@link URL} for this resource
+   */
+  Optional<URL> findIn(String resource, String groupId, String artifactId, Optional<String> version, String classifier,
+                       String type, Object caller);
+
+  /**
+   * Same as {@link #findIn(String, String, String, Optional, String, String, Object)} but using no classifier and {@code jar} as
+   * type.
    *
    * @param resource the name of the resource to load
    * @param groupId the group ID of the artifact where to look
@@ -62,6 +97,7 @@ public interface ResourceLocator {
    * @param caller the calling object
    * @return the {@link URL} for this resource
    */
-  Optional<URL> findIn(String resource, String groupId, String artifactId, Optional<String> version, Object caller);
-
+  default Optional<URL> findIn(String resource, String groupId, String artifactId, Optional<String> version, Object caller) {
+    return findIn(resource, groupId, artifactId, version, "", "jar", caller);
+  }
 }
