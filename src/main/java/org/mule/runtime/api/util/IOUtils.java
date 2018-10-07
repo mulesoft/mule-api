34677fb0133798e757578f08722211e5a72e5af0
pablolagreca <Neruda10>
2018-10-07 12:38:15 -0300

    MULE-15450 - Remove application model from runtime - part 1

--- src/main/java/org/mule/runtime/api/util/IOUtils.java
+++ src/main/java/org/mule/runtime/api/util/IOUtils.java
@@ -6,18 +6,33 @@
  */
 package org.mule.runtime.api.util;
 
+import static org.mule.runtime.internal.util.FileUtils.newFile;
+import org.mule.runtime.internal.util.ClassUtils;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Mule API I/O Utilities.
+ * 
  * @since 1.2.0
  */
 public class IOUtils {
 
+  private static final Logger logger = LoggerFactory.getLogger(IOUtils.class);
+
   /**
    * This method wraps {@link org.apache.commons.io.IOUtils}' <code>toByteArray(InputStream)</code> method but catches any
    * {@link IOException} and wraps it into a {@link RuntimeException}.
+   * 
    * @param input
    * @return the byte array representation of the stream.
    */
@@ -28,4 +43,67 @@ public class IOUtils {
       throw new RuntimeException(iox);
     }
   }
+
+  /**
+   * Attempts to load a resource from the file system or from the classpath, in that order.
+   *
+   * @param resourceName The name of the resource to load
+   * @param callingClass The Class object of the calling object
+   * @return an URL to the resource or null if resource not found
+   */
+  public static URL getResourceAsUrl(final String resourceName, final Class callingClass) {
+    return getResourceAsUrl(resourceName, callingClass, true, true);
+  }
+
+  /**
+   * Attempts to load a resource from the file system or from the classpath, in that order.
+   *
+   * @param resourceName The name of the resource to load
+   * @param callingClass The Class object of the calling object
+   * @param tryAsFile - try to load the resource from the local file system
+   * @param tryAsUrl - try to load the resource as a Url string
+   * @return an URL to the resource or null if resource not found
+   */
+  public static URL getResourceAsUrl(final String resourceName, final Class callingClass, boolean tryAsFile, boolean tryAsUrl) {
+    if (resourceName == null) {
+      throw new IllegalArgumentException("resourceName cannot be null");
+    }
+    URL url = null;
+
+    // Try to load the resource from the file system.
+    if (tryAsFile) {
+      try {
+        File file = newFile(resourceName);
+        if (file.exists()) {
+          url = file.getAbsoluteFile().toURL();
+        } else {
+          logger.debug("Unable to load resource from the file system: " + file.getAbsolutePath());
+        }
+      } catch (Exception e) {
+        logger.debug("Unable to load resource from the file system: " + e.getMessage());
+      }
+    }
+
+    // Try to load the resource from the classpath.
+    if (url == null) {
+      try {
+        url = (URL) AccessController.doPrivileged((PrivilegedAction) () -> ClassUtils.getResource(resourceName, callingClass));
+        if (url == null) {
+          logger.debug("Unable to load resource " + resourceName + " from the classpath");
+        }
+      } catch (Exception e) {
+        logger.debug("Unable to load resource " + resourceName + " from the classpath: " + e.getMessage());
+      }
+    }
+
+    if (url == null) {
+      try {
+        url = new URL(resourceName);
+      } catch (MalformedURLException e) {
+        // ignore
+      }
+    }
+    return url;
+  }
+
 }
