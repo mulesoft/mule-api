f276317b34fd967d57f636054a963012da15831b
pablolagreca <pablolagreca@hotmail.com>
2018-10-10 11:05:57 -0300

    MULE-15450 - Remove application model from runtime - part 1 (#522)

---
+++ src/main/java/org/mule/runtime/internal/util/ClassUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.internal.util;
+
+import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+public class ClassUtils {
+
+  /**
+   * Load a given resource.
+   * <p/>
+   * This method will try to load the resource using the following methods (in order):
+   * <ul>
+   * <li>From {@link Thread#getContextClassLoader() Thread.currentThread().getContextClassLoader()}
+   * <li>From {@link Class#getClassLoader() ClassUtils.class.getClassLoader()}
+   * <li>From the {@link Class#getClassLoader() callingClass.getClassLoader() }
+   * </ul>
+   *
+   * @param resourceName The name of the resource to load
+   * @param callingClass The Class object of the calling object
+   * @return A URL pointing to the resource to load or null if the resource is not found
+   */
+  public static URL getResource(final String resourceName, final Class<?> callingClass) {
+    URL url = AccessController.doPrivileged((PrivilegedAction<URL>) () -> {
+      final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+      return cl != null ? cl.getResource(resourceName) : null;
+    });
+
+    if (url == null) {
+      url = AccessController
+          .doPrivileged((PrivilegedAction<URL>) () -> ClassUtils.class.getClassLoader().getResource(resourceName));
+    }
+
+    if (url == null) {
+      url = AccessController.doPrivileged((PrivilegedAction<URL>) () -> callingClass.getClassLoader().getResource(resourceName));
+    }
+
+    return url;
+  }
+
+}
---
+++ src/main/java/org/mule/runtime/internal/util/FileUtils.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.internal.util;
+
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import org.mule.runtime.api.exception.MuleRuntimeException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUtils {
+
+  /**
+   * Workaround for JDK bug <a href="http://bugs.sun.com/bugdatabase/view_bug.do;:YfiG?bug_id=4117557"> 4117557</a>. More
+   * in-context information at <a href="http://mule.mulesoft.org/jira/browse/MULE-1112">MULE-1112</a>
+   * <p/>
+   * Factory methods correspond to constructors of the <code>java.io.File class</code>. No physical file created in this method.
+   *
+   * @see File
+   */
+  public static File newFile(String pathName) {
+    try {
+      return new File(pathName).getCanonicalFile();
+    } catch (IOException e) {
+      throw new MuleRuntimeException(createStaticMessage("Unable to create a canonical file for " + pathName),
+                                     e);
+    }
+  }
+
+}
