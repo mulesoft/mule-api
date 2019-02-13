0896746aeeadd5434178702cf165f612edbf03c6
Martin Buchwald <martin.buchwald@mulesoft.com>
2019-02-13 15:00:02 -0300

    MULE-15929: Interception API - Allow to fail processor with specific message

--- src/main/java/org/mule/runtime/api/interception/InterceptionAction.java
+++ src/main/java/org/mule/runtime/api/interception/InterceptionAction.java
@@ -76,4 +76,21 @@ public interface InterceptionAction {
    *         {@link ProcessorInterceptor#around(java.util.Map, InterceptionEvent, InterceptionAction) around()} method.
    */
   CompletableFuture<InterceptionEvent> fail(ErrorType errorType);
+
+  /**
+   * Interrupts the current interception chain, not running the next {@link ProcessorInterceptor interceptors} in the chain and
+   * the intercepted component.
+   * <p>
+   * A future is returned which in turn it will fail with an exception that maps the provided error type, with the
+   * error message provided.
+   * <p>
+   * If called for a <b>router</b> component, the whole route (and all the components contained in it) will be skipped.
+   * <p>
+   * If called for an intercepting component (i.e. a {@code splitter}), the listener components (i.e. what follows the
+   * {@code splitter} up to an {@code aggregator}) are skipped as well.
+   *
+   * @return a {@link CompletableFuture} using the same {@link InterceptionEvent event} passed to the
+   *         {@link ProcessorInterceptor#around(java.util.Map, InterceptionEvent, InterceptionAction) around()} method.
+   */
+  CompletableFuture<InterceptionEvent> fail(ErrorType errorType, String msg);
 }
