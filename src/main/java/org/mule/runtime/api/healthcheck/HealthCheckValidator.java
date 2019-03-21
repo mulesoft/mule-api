fc0e53443b2e0f5f647019791353e5399e060c7f
svacas <svacas@gmail.com>
2019-03-21 17:50:22 -0300

    MULE-16565: Add support for Health Check validation (#560)
    (cherry picked from commit a91dea26b961ebca247866916964f59b2eeeb702)

---
+++ src/main/java/org/mule/runtime/api/healthcheck/HealthCheckValidator.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.healthcheck;
+
+/**
+ * HealthCheck validator contract to be implemented by any component
+ * that needs to report on the ready status of an application.
+ *
+ * The validator has to be added to the application registry in order
+ * to be discoverable.
+ *
+ */
+public interface HealthCheckValidator {
+
+  /**
+   * @return a {@link ReadyStatus} instance that informs if the application is ready
+   *  to receive traffic or not
+   */
+  ReadyStatus ready();
+}
