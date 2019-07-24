f682441555a80cd84f67baff8f5fdd3fe85ee23d
facunlk <facundo.lopez.kaufmann@gmail.com>
2019-07-24 17:33:59 -0300

    MULE-17240: changed MultiMap constructor to keep input order

--- src/test/java/org/mule/runtime/api/util/MultiMapTestCase.java
+++ src/test/java/org/mule/runtime/api/util/MultiMapTestCase.java
@@ -15,9 +15,11 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.hamcrest.Description;
@@ -93,6 +95,16 @@ public class MultiMapTestCase {
   @Test(expected = UnsupportedOperationException.class)
   public void immutableMultiMapFailsOnClear() {
     multiMap.toImmutableMultiMap().clear();
+  }
+
+  @Test
+  public void toImmutableMapKeepsOrder() {
+    multiMap.put(KEY_3, VALUE_1);
+    multiMap.put(KEY_2, VALUE_1);
+    multiMap.put(KEY_1, VALUE_1);
+    List<Map.Entry<String, String>> entryList = multiMap.entryList();
+    List<Map.Entry<String, String>> immutableEntryList = multiMap.toImmutableMultiMap().entryList();
+    assertEquals(entryList, immutableEntryList);
   }
 
   @Test
