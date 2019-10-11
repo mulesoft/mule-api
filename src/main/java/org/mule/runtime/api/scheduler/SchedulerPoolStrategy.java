ea236426ed76576af0f359a82d34a0a693d661f3
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-10-11 13:31:21 -0300

    code cleanup

--- src/main/java/org/mule/runtime/api/scheduler/SchedulerPoolStrategy.java
+++ src/main/java/org/mule/runtime/api/scheduler/SchedulerPoolStrategy.java
@@ -6,7 +6,21 @@
  */
 package org.mule.runtime.api.scheduler;
 
+/**
+ * The strategy that the {@link SchedulerService} should use to provision the thread pools that back the three main types
+ * of schedulers
+ *
+ * @since 1.3.0
+ */
 public enum SchedulerPoolStrategy {
 
-  UBER, DEDICATED
+  /**
+   * The three scheduler types are backed by one unique thread pool.
+   */
+  UBER,
+
+  /**
+   * Each scheduler type is backed by its own thread pool
+   */
+  DEDICATED
 }
--- src/main/java/org/mule/runtime/api/scheduler/SchedulerPoolsConfig.java
+++ src/main/java/org/mule/runtime/api/scheduler/SchedulerPoolsConfig.java
@@ -20,6 +20,10 @@ import java.util.concurrent.Executor;
 @NoImplement
 public interface SchedulerPoolsConfig {
 
+  /**
+   * @return The {@link SchedulerPoolStrategy} to be used
+   * @since 1.3.0
+   */
   SchedulerPoolStrategy getSchedulerPoolStrategy();
 
   /**
@@ -29,66 +33,90 @@ public interface SchedulerPoolsConfig {
   OptionalLong getGracefulShutdownTimeout();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the number of threads to keep in the {@code cpu_lite} pool, even if they are idle.
    */
   OptionalInt getCpuLightPoolSize();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the size of the queue to use for holding {@code cpu_lite} tasks before they are executed.
    */
   OptionalInt getCpuLightQueueSize();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the number of threads to keep in the {@code I/O} pool.
    */
   OptionalInt getIoCorePoolSize();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the maximum number of threads to allow in the {@code I/O} pool.
    */
   OptionalInt getIoMaxPoolSize();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the size of the queue to use for holding {@code I/O} tasks before they are executed.
    */
   OptionalInt getIoQueueSize();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return when the number of threads in the {@code I/O} pool is greater than {@link #getIoCorePoolSize()}, this is the maximum
    *         time (in milliseconds) that excess idle threads will wait for new tasks before terminating.
    */
   OptionalLong getIoKeepAlive();
 
   /**
-   * @return the number of threads to keep in the {@code I/O} pool.
-   */
-  OptionalInt getUberCorePoolSize();
-
-  /**
-   * @return the maximum number of threads to allow in the {@code I/O} pool.
-   */
-  OptionalInt getUberMaxPoolSize();
-
-  /**
-   * @return the size of the queue to use for holding {@code I/O} tasks before they are executed.
-   */
-  OptionalInt getUberQueueSize();
-
-  /**
-   * @return when the number of threads in the {@code I/O} pool is greater than {@link #getIoCorePoolSize()}, this is the maximum
-   *         time (in milliseconds) that excess idle threads will wait for new tasks before terminating.
-   */
-  OptionalLong getUberKeepAlive();
-
-  /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the number of threads to keep in the {@code cpu_intensive} pool, even if they are idle.
    */
   OptionalInt getCpuIntensivePoolSize();
 
   /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#DEDICATED}
+   *
    * @return the size of the queue to use for holding {@code cpu_intensive} tasks before they are executed.
    */
   OptionalInt getCpuIntensiveQueueSize();
+
+  /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#UBER}
+   *
+   * @return the number of threads to keep in the {@code UBER} pool.
+   */
+  OptionalInt getUberCorePoolSize();
+
+  /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#UBER}
+   *
+   * @return the maximum number of threads to allow in the {@code UBER} pool.
+   */
+  OptionalInt getUberMaxPoolSize();
+
+  /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#UBER}
+   *
+   * @return the size of the queue to use for holding {@code UBER} tasks before they are executed.
+   */
+  OptionalInt getUberQueueSize();
+
+  /**
+   * Only applies when {@link #getSchedulerPoolStrategy()} is {@link SchedulerPoolStrategy#UBER}
+   *
+   * @return when the number of threads in the {@code Uber} pool is greater than {@link #getUberCorePoolSize()}, this is the maximum
+   *         time (in milliseconds) that excess idle threads will wait for new tasks before terminating.
+   */
+  OptionalLong getUberKeepAlive();
 
   /**
    * @return the prefix to prepend to the names of the threads of the pools created for a scheduler with this configuration.
