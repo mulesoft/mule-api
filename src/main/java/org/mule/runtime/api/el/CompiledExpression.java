7372718fc12215a4076d2b6a1b19bdb31d044979
machaval <mariano.achaval@mulesoft.com>
2019-11-25 17:02:03 -0300

    MULE-17782. Add support for compile method on ExpressionLanguage

---
+++ src/main/java/org/mule/runtime/api/el/CompiledExpression.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.el;
+
+import org.mule.runtime.api.metadata.MediaType;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Represents a compiled expression
+ *
+ * @since 1.3
+ */
+public interface CompiledExpression {
+
+  /**
+   * Returns the expression that generates this compiled expression
+   *
+   * @return The expression
+   */
+  String expression();
+
+
+  /**
+   * Returns the output type of this compiled expression
+   *
+   * @return The output type
+   */
+  Optional<MediaType> outputType();
+
+
+  /**
+   * Returns the list of external variable references
+   *
+   * @return The list of external variable references
+   */
+  List<ModuleElementName> externalDependencies();
+
+
+}
---
+++ src/main/java/org/mule/runtime/api/el/ModuleElementName.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.el;
+
+import java.util.Objects;
+
+/**
+ * Represents the name of an Element in a given module
+ *
+ * @since 1.3
+ */
+public class ModuleElementName {
+
+  private ModuleNamespace namespace;
+  private String localName;
+
+  public ModuleElementName(ModuleNamespace namespace, String localName) {
+    this.namespace = namespace;
+    this.localName = localName;
+  }
+
+
+  public ModuleNamespace getNamespace() {
+    return namespace;
+  }
+
+  public String getLocalName() {
+    return localName;
+  }
+
+  @Override
+  public String toString() {
+    return namespace + "::" + localName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    ModuleElementName that = (ModuleElementName) o;
+    return Objects.equals(namespace, that.namespace) &&
+        Objects.equals(localName, that.localName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(namespace, localName);
+  }
+}
