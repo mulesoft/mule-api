510f5943cd8a72f35e0b5f8483b6a9f54c1f99ff
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-11-27 18:56:17 -0300

    fixes + tests

---
+++ src/test/java/org/mule/runtime/api/util/collection/FastMapTestCase.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.util.collection;
+
+import static java.lang.String.valueOf;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class FastMapTestCase {
+
+  private static final String[] KEYS = new String[] {"one", "two", "three", "four", "five", "six", "seven"};
+  private static final String[] VALUES = new String[] {"uno", "dos", "tres", "cuatro", "cinco", "seis", "siete"};
+
+  @Parameters(name = "with Size: {0}")
+  public static Iterable<Integer> data() {
+    return asList(KEYS.length, 5);
+  }
+
+  private final int mapSize;
+  private Map<String, String> map = new FastMap<>();
+
+  public FastMapTestCase(int mapSize) {
+    this.mapSize = mapSize;
+  }
+
+  @Before
+  public void before() {
+    assertThat(map.size(), is(0));
+    assertThat(map.isEmpty(), is(true));
+
+    populate(mapSize);
+  }
+
+  private void populate(int size) {
+    map.clear();
+
+    for (int i = 0; i < size; i++) {
+      map.put(KEYS[i], VALUES[i]);
+      assertThat(map.size(), is(i + 1));
+      assertThat(map.containsKey(KEYS[i]), is(true));
+      assertThat(map.isEmpty(), is(false));
+    }
+  }
+
+  @Test
+  public void putRepeatedKeys() {
+    for (int i = 0; i < mapSize; i++) {
+      map.put(KEYS[i], VALUES[i]);
+      assertThat(map.size(), is(mapSize));
+      assertThat(map.containsKey(KEYS[i]), is(true));
+      assertThat(map.isEmpty(), is(false));
+    }
+  }
+
+  @Test
+  public void removeKeysInRandomOrder() {
+    Random random = new Random();
+    Set<Integer> removedKeyIndex;
+
+    for (int i = 0; i < 10; i++) {
+      populate(mapSize);
+      removedKeyIndex = new HashSet<>();
+
+      while (!map.isEmpty()) {
+        int index;
+        do {
+          index = random.nextInt(mapSize);
+        } while (!removedKeyIndex.add(index));
+
+        assertThat(map.remove(KEYS[index]), is(VALUES[index]));
+        assertThat(map.isEmpty(), is(removedKeyIndex.size() == mapSize));
+        assertThat(map.size(), is(mapSize - removedKeyIndex.size()));
+      }
+    }
+
+    assertThat(map.isEmpty(), is(true));
+    assertThat(map.size(), is(0));
+  }
+
+  @Test
+  public void putAll() {
+    map.clear();
+    
+    String originalKey = "original";
+    String originalValue = "value";
+
+    map.put(originalKey, originalValue);
+    Map<String, String> other = new HashMap<>();
+
+    int otherMapSize = 5;
+    for (int i = 0; i < otherMapSize; i++) {
+      other.put(valueOf(i), "_" + i);
+    }
+
+    other.put(originalKey, originalValue);
+
+    map.putAll(other);
+    assertThat(map.size(), is(otherMapSize + 1));
+    assertThat(map.isEmpty(), is(false));
+
+    for (int i = 0; i < otherMapSize; i++) {
+      final String key = valueOf(i);
+      final String value = "_" + i;
+
+      assertThat(map.containsKey(key), is(true));
+      assertThat(map.containsValue(value), is(true));
+      assertThat(map.get(key), equalTo(value));
+    }
+
+    assertThat(map.containsKey(originalKey), is(true));
+    assertThat(map.containsValue(originalValue), is(true));
+    assertThat(map.get(originalKey), equalTo(originalValue));
+  }
+
+  @Test
+  public void clear() {
+    map.clear();
+    assertThat(map.isEmpty(), is(true));
+    assertThat(map.size(), is(0));
+
+    populate(mapSize);
+  }
+
+  @Test
+  public void entrySet() {
+    map.entrySet().forEach(entry -> {
+      int i = getKeyIndex(entry.getKey());
+      assertThat(entry.getValue(), equalTo(VALUES[i]));
+    });
+  }
+
+  @Test
+  public void values() {
+    Collection<String> values = map.values();
+    assertThat(values, hasSize(mapSize));
+    for (int i = 0; i < mapSize; i++) {
+      assertThat(values.contains(VALUES[i]), is(true));
+    }
+  }
+
+  @Test
+  public void mutateEntry() {
+    map.entrySet().forEach(entry -> entry.setValue(entry.getValue().toUpperCase()));
+
+    for (int i = 0; i < map.size(); i++) {
+      assertThat(map.get(KEYS[i]), equalTo(VALUES[i].toUpperCase()));
+    }
+  }
+
+  private int getKeyIndex(String key) {
+    for (int i = 0; i < KEYS.length; i++) {
+      if (KEYS[i].equals(key)) {
+        return i;
+      }
+    }
+
+    throw new NoSuchElementException();
+  }
+}
