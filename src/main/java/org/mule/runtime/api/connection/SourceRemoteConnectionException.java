bfc9a7e805717155fc3e684029f9a183594cc620
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2020-04-24 17:42:54 -0300

    MULE-18343: Source is not notified of failure on ConnectionException in (#653)
    (cherry picked from commit 755e0516451fa1fe9104ff13af9b2b4c00a5591b)

---
+++ src/main/java/org/mule/runtime/api/connection/SourceRemoteConnectionException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.connection;
+
+/**
+ * Signals that a remote error in the connection occurred related to the source
+ *
+ * @since 4.3.0, 4.2.3
+ */
+public class SourceRemoteConnectionException extends RemoteConnectionException {
+
+  public SourceRemoteConnectionException(String message) {
+    super(message);
+  }
+
+  public SourceRemoteConnectionException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
