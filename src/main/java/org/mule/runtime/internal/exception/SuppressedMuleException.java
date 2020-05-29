9adf142b2b3c8f7a708f53461818b865e5a3b4ba
Ivan Fritzler <ifritzler@mulesoft.com>
2020-05-28 22:42:23 -0300

    MULE-18041: Code format

--- src/main/java/org/mule/runtime/internal/exception/SuppressedMuleException.java
+++ src/main/java/org/mule/runtime/internal/exception/SuppressedMuleException.java
@@ -73,9 +73,12 @@ public class SuppressedMuleException extends MuleException {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    if (!super.equals(o))
+      return false;
     SuppressedMuleException that = (SuppressedMuleException) o;
     return suppressedException.equals(that.suppressedException);
   }
