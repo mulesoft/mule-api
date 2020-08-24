9a20bb2995d5c882653beaf89b4b58acb56d9f20
mlischetti <lischettim@gmail.com>
2020-08-24 18:35:53 -0300

    Add sytem property

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -160,9 +160,16 @@ public final class MuleSystemProperties {
    * It can be used for troubleshooting purposes (for example, if someone tries to call {@link CursorProvider#openCursor()} on
    * an already closed cursor.
    *
-   * @since 1.3.0
+   * @since 1.4.0
    */
   public static final String TRACK_CURSOR_PROVIDER_CLOSE_PROPERTY = SYSTEM_PROPERTY_PREFIX + "track.cursorProvider.close";
+
+  /**
+   * When enabled this System Property.
+   *
+   * @since 1.3.0
+   */
+  public static final String STREAMING_VERBOSE_PROPERTY = SYSTEM_PROPERTY_PREFIX + "streaming.verbose";
 
   /**
    * When enabled this System Property, if an ErrorType is not found in the policy ErrorType repository, then
