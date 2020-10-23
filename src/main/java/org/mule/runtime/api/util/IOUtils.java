89ba7d46500f11b58f99c0330877c1e2e4ccd4c2
Julieta <luzzijulieta@gmail.com>
2020-10-23 15:38:29 -0300

    MULE-1892: HTTP-SERVICE bug with jdk8u265-b01 and Runtime 4.1.6 in EB… (#731)
    MULE-1892: HTTP-SERVICE bug with jdk8u265-b01 and Runtime 4.1.6 in EBS 12.2 Conn

---
+++ src/main/java/org/mule/runtime/api/util/IOUtils.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Mule API I/O Utilities.
+ * @since 1.1.6
+ */
+public class IOUtils {
+
+  /**
+   * This method wraps {@link org.apache.commons.io.IOUtils}' <code>toByteArray(InputStream)</code> method but catches any
+   * {@link IOException} and wraps it into a {@link RuntimeException}.
+   * @param input
+   * @return the byte array representation of the stream.
+   */
+  public static byte[] toByteArray(InputStream input) {
+    try {
+      return org.apache.commons.io.IOUtils.toByteArray(input);
+    } catch (IOException iox) {
+      throw new RuntimeException(iox);
+    }
+  }
+}
