c68da462a494889003741cc5d416665bf41f432e
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-01-12 09:48:21 -0300

    MULE-19038: feature flag reserved variable names (#748) (#758)
    (cherry picked from commit 7f8487ad6b222233f0b6982a3329c8aa129e1f89)
    (cherry picked from commit 34e16635d6a84396930e0279cd6f0a2769cacf22)

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -6,9 +6,11 @@
  */
 package org.mule.runtime.api.util;
 
+import static java.lang.System.getProperty;
+
+import org.mule.runtime.api.config.MuleRuntimeFeature;
 import org.mule.runtime.api.streaming.CursorProvider;
 
-import static java.lang.System.getProperty;
 
 /**
  * Defines Mule System properties.
@@ -143,6 +145,15 @@ public final class MuleSystemProperties {
    * @since 4.4.0, 4.3.1, 4.2.3
    */
   public static final String MULE_PRINT_STACK_TRACES_ON_DROP = SYSTEM_PROPERTY_PREFIX + "fluxSinkRecorder.printStackTracesOnDrop";
+
+  /**
+   * When set to {@code true} an application won't be able to override reserved properties such as <code>app.name</code>. If it
+   * isn't set, the behaviour will depend on {@link MuleRuntimeFeature#HONOUR_RESERVED_PROPERTIES}.
+   * 
+   * @see MuleRuntimeFeature#HONOUR_RESERVED_PROPERTIES
+   * @since 4.4.0, 4.3.1, 4.2.3
+   */
+  public static final String HONOUR_RESERVED_PROPERTIES_PROPERTY = SYSTEM_PROPERTY_PREFIX + "honour.reserved.properties";
 
   /**
    * @return {@code true} if the {@link #TESTING_MODE_PROPERTY_NAME} property has been set (regardless of the value)
