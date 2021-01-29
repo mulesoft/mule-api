49fc4514ca49bd65e2d3724564c32914e1bb67af
Mariano Lischetti <lischettim@gmail.com>
2021-01-29 15:21:54 -0300

    MULE-19180: Event internal parameter StackOverflow error while buildi… (#782)

---
+++ src/main/java/org/mule/runtime/internal/util/collection/UnmodifiableMap.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.internal.util.collection;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * An unmodifiable view of the specified map. 
+ * 
+ * @param <K> the generic type of the keys
+ * @param <V> the generic type of the values
+ * @since 1.4, 1.3.1
+ */
+public class UnmodifiableMap<K, V> implements Map<K, V>, Serializable {
+
+  private static final long serialVersionUID = -1657860614763056722L;
+
+  private final Map<K, V> delegate;
+
+  /**
+   * Returns an unmodifiable view of the given {@code map}.
+   * This method avoid create a new {@link UnmodifiableMap} instance if current {@code map} it's an {@link UnmodifiableMap}
+   *
+   * @param <K> the class of the map keys
+   * @param <V> the class of the map values
+   * @param map the map for which an unmodifiable view is to be returned.
+   * @return an unmodifiable view of the specified map.
+   */
+  public static <K, V> Map<K, V> unmodifiableMap(Map<K, V> map) {
+    if (map instanceof UnmodifiableMap) {
+      return map;
+    } else {
+      return new UnmodifiableMap(map);
+    }
+  }
+
+  private UnmodifiableMap(Map<K, V> m) {
+    this.delegate = Collections.unmodifiableMap(m);
+  }
+
+  @Override
+  public int size() {
+    return delegate.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return delegate.isEmpty();
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return delegate.containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    return delegate.containsValue(value);
+  }
+
+  @Override
+  public V get(Object key) {
+    return delegate.get(key);
+  }
+
+  @Override
+  public V put(K key, V value) {
+    return delegate.put(key, value);
+  }
+
+  @Override
+  public V remove(Object key) {
+    return delegate.remove(key);
+  }
+
+  @Override
+  public void putAll(Map<? extends K, ? extends V> m) {
+    delegate.putAll(m);
+  }
+
+  @Override
+  public void clear() {
+    delegate.clear();
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return delegate.keySet();
+  }
+
+  @Override
+  public Collection<V> values() {
+    return delegate.values();
+  }
+
+  @Override
+  public Set<Entry<K, V>> entrySet() {
+    return delegate.entrySet();
+  }
+
+  @Override
+  public int hashCode() {
+    return delegate.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return delegate.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return delegate.toString();
+  }
+
+  @Override
+  public V getOrDefault(Object key, V defaultValue) {
+    return delegate.getOrDefault(key, defaultValue);
+  }
+
+  @Override
+  public void forEach(BiConsumer<? super K, ? super V> action) {
+    delegate.forEach(action);
+  }
+
+  @Override
+  public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+    delegate.replaceAll(function);
+  }
+
+  @Override
+  public V putIfAbsent(K key, V value) {
+    return delegate.putIfAbsent(key, value);
+  }
+
+  @Override
+  public boolean remove(Object key, Object value) {
+    return delegate.remove(key, value);
+  }
+
+  @Override
+  public boolean replace(K key, V oldValue, V newValue) {
+    return delegate.replace(key, oldValue, newValue);
+  }
+
+  @Override
+  public V replace(K key, V value) {
+    return delegate.replace(key, value);
+  }
+
+  @Override
+  public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
+    return delegate.computeIfAbsent(key, mappingFunction);
+  }
+
+  @Override
+  public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+    return delegate.computeIfPresent(key, remappingFunction);
+  }
+
+  @Override
+  public V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+    return delegate.compute(key, remappingFunction);
+  }
+
+  @Override
+  public V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+    return delegate.merge(key, value, remappingFunction);
+  }
+}
