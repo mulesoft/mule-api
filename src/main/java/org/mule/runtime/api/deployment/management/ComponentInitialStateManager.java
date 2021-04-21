7363746d17ed6d64fdc4481ce0735d298e19b983
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-04-21 14:13:04 -0300

    MULE-19356: Avoid managing source lifecycle from flow when it has no source (#816)

--- src/main/java/org/mule/runtime/api/deployment/management/ComponentInitialStateManager.java
+++ src/main/java/org/mule/runtime/api/deployment/management/ComponentInitialStateManager.java
@@ -8,6 +8,7 @@ package org.mule.runtime.api.deployment.management;
 
 import org.mule.api.annotation.NoImplement;
 import org.mule.runtime.api.component.Component;
+import org.mule.runtime.api.util.MuleSystemProperties;
 
 /**
  * Manages the initial state of components within a mule artifact (application, domain, policy, etc).
@@ -18,10 +19,11 @@ public interface ComponentInitialStateManager {
   /**
    * This is a configuration property that can be set at deployment time to disable the scheduler message sources to be started
    * when deploying an application.
-   * 
+   *
    * @deprecated Please use {@link org.mule.runtime.api.util.MuleSystemProperties#DISABLE_SCHEDULER_SOURCES_PROPERTY} instead.
    */
-  String DISABLE_SCHEDULER_SOURCES_PROPERTY = "mule.config.scheduler.disabled";
+  @Deprecated
+  String DISABLE_SCHEDULER_SOURCES_PROPERTY = MuleSystemProperties.DISABLE_SCHEDULER_SOURCES_PROPERTY;
 
   /**
    * Service ID to use to customize the {@link ComponentInitialStateManager} instance for the application through the use of the
@@ -32,7 +34,7 @@ public interface ComponentInitialStateManager {
   /**
    * Manages the initial state of sources. This method will be called before starting the source for the first time to know if
    * it's initial state must be started or not.
-   * 
+   *
    * @param component the source component to be started
    * @return true if the source must be started, false otherise.
    */
