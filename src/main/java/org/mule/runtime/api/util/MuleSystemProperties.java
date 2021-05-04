9caeb98d0b5e70d53b47d40547cf8303d0fb21e1
Ivan Fritzler <ifritzler@mulesoft.com>
2021-05-04 15:46:52 -0300

    Javadoc

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -350,7 +350,9 @@ public final class MuleSystemProperties {
       "resolve.execution.mode.based.on.async.reconnection.strategy";
 
   /**
-   * If set to true, ...
+   * If set to true, extensions imported by a policy will be managed in complete isolation from the extensions imported by the
+   * application that is being applied to, and validations will prevent the usage of explicit configurations declared by the
+   * application as part of the policy initialization."
    *
    * @since 4.4.0, 4.3.1
    */
