023e54a809d04556d4d0f88426e9ebb0df4e687a
Ivan Fritzler <58004191+IvanAndresFritzler@users.noreply.github.com>
2021-08-11 09:12:02 -0300

    MULE-19496: Feature flags documentation is not complete

--- src/main/java/org/mule/runtime/api/config/MuleRuntimeFeature.java
+++ src/main/java/org/mule/runtime/api/config/MuleRuntimeFeature.java
@@ -17,22 +17,16 @@ import static org.mule.runtime.api.util.MuleSystemProperties.SET_VARIABLE_WITH_N
 import static org.mule.runtime.api.util.MuleSystemProperties.TO_STRING_TRANSFORMER_TRANSFORM_ITERATOR_ELEMENTS_PROPERTY;
 import static org.mule.runtime.api.util.MuleSystemProperties.ENTITY_RESOLVER_FAIL_ON_FIRST_ERROR_PROPERTY;
 
-import org.mule.runtime.api.util.MuleSystemProperties;
-
 import java.util.Optional;
 
 /**
  * <p>
- * List of {@link Feature}s that can be configured to be enabled or disabled per application depending on the deployment context.
+ * List of {@link Feature}s that will be enabled or disabled per application depending on the deployment context.
  * </p>
  *
  * <p>
  * When some Mule runtime feature needs to be flagged, it should be added here as a new enum constant. Each entry must have a
- * meaningful name, clear enough to understand what it represents, a description with enough information to know how the runtime
- * will work whether the feature is enabled or disabled, the issue that motivated this feature, and a list of Runtime versions
- * from when it exists. Each enum constant has to have its Javadoc with further information about how the feature is configured.
- * </p>
- *
+ * meaningful, customer facing name, and must provide values for all the {@link Feature} fields.
  * <p>
  * For example:
  * </p>
@@ -42,14 +36,12 @@ import java.util.Optional;
  *      ...
  *
  *     {@code /}**
- *       * An application shouldn't override reserved properties, but it was possible until MULE-17659. This behaviour has
- *       * to be fixed by default to any application developed for 4.3.0+ Runtime but can be overridden by setting
- *       * &lcub;@link MuleSystemProperties#HONOUR_RESERVED_PROPERTIES_PROPERTY&rcub; System Property.
+ *       * When enabled, reserved properties such as 'app.name' can't be overridden by global properties (overrides will be ignored).
  *       *
- *       * @since 4.4.0, 4.3.1
+ *       * @since 4.4.0, 4.3.0-202102
  *       *
  *      {@code *}/
- *      HONOUR_RESERVED_PROPERTIES("Whether reserved properties such as 'app.name' can't be overridden by global properties.",
+ *      HONOUR_RESERVED_PROPERTIES("When enabled, reserved properties such as 'app.name' can't be overridden by global properties (overrides will be ignored).",
  *            "MULE-19083", "4.4.0, 4.3.1", HONOUR_RESERVED_PROPERTIES_PROPERTY),
  *
  *      ...
@@ -57,118 +49,118 @@ import java.util.Optional;
  *    }
  * </pre>
  *
+ * @see Feature
+ * @see FeatureFlaggingService
  * @since 4.4.0 4.3.1
  */
 public enum MuleRuntimeFeature implements Feature {
 
   /**
-   * An application shouldn't override reserved properties, but it was possible until MULE-17659. This behaviour has to be fixed
-   * by default to any application developed for 4.3.0+ Runtime but can be overridden by setting
-   * {@link MuleSystemProperties#HONOUR_RESERVED_PROPERTIES_PROPERTY} System Property.
+   * When enabled, reserved properties such as 'app.name' can't be overridden by global properties (overrides will be ignored).
    *
-   * @since 4.4.0, 4.3.1
+   * @since 4.4.0, 4.3.0-202102
    */
-  HONOUR_RESERVED_PROPERTIES("Whether reserved properties such as 'app.name' can't be overridden by global properties.",
-      "MULE-19038", "4.4.0, 4.3.1", HONOUR_RESERVED_PROPERTIES_PROPERTY),
+  HONOUR_RESERVED_PROPERTIES(
+      "When enabled, reserved properties such as 'app.name' can't be overridden by global properties (overrides will be ignored).",
+      "MULE-17659", "4.4.0, 4.3.0", HONOUR_RESERVED_PROPERTIES_PROPERTY),
 
   /**
-   * If set to true, then DW will correctly handle Splitter's exceptions, avoiding some serialization issues. Be aware that when
-   * enabled, this can make {@code error.cause} return a different exception. For more information see DW-383.
+   * When enabled, DataWeave will correctly handle splitter exceptions, avoiding Scala serialization issues.
    *
-   * @since 4.4.0, 4.3.1, 4.2.3
+   * @since 4.4.0
    */
   HANDLE_SPLITTER_EXCEPTION(
-      "If set to true, then DW will correctly handle Splitter's exceptions, avoiding some serialization issues.",
+      "When enabled, DataWeave will correctly handle splitter exceptions, avoiding Scala serialization issues.",
       "DW-383",
-      "4.4.0, 4.3.1, 4.2.3", HANDLE_SPLITTER_EXCEPTION_PROPERTY),
+      "4.4.0", HANDLE_SPLITTER_EXCEPTION_PROPERTY),
 
   /**
-   * If set to true, then fixed batch aggregator will only commit when a full block is processed. For more information see EE-7443
+   * When enabled, fixed batch aggregators will only commit when a full block is processed. See EE-7443 for more information.
    *
-   * @since 4.4.0, 4.3.1, 4.2.3
+   * @since 4.4.0, 4.3.0-202103, 4.2.2-202103"
    */
   BATCH_FIXED_AGGREGATOR_TRANSACTION_RECORD_BUFFER(
-      "If set to true, then fixed batch aggregator will only commit when a full block is processed.",
-      "EE-7443",
-      "4.4.0, 4.3.1, 4.2.3", BATCH_FIXED_AGGREGATOR_TRANSACTION_RECORD_BUFFER_PROPERTY),
+      "When enabled, fixed batch aggregators will only commit when a full block is processed. See EE-7443 for more information.",
+      "MULE-19218",
+      "", BATCH_FIXED_AGGREGATOR_TRANSACTION_RECORD_BUFFER_PROPERTY),
 
   /**
-   * If set to true, the connection errors will be taken into account to trigger alerts.
+   * When enabled, Connection errors will be computed as part of alerts triggering.
    *
-   * @since 4.4.0, 4.3.1
+   * @since 4.4.0, 4.3.0-202103
    */
   COMPUTE_CONNECTION_ERRORS_IN_STATS(
-      "If set to true, the connection errors will be computed to trigger alerts.",
-      "DW-383",
-      "4.4.0, 4.3.1", COMPUTE_CONNECTION_ERRORS_IN_STATS_PROPERTY),
+      "When enabled, Connection errors will be computed as part of alerts triggering.",
+      "MULE-19020",
+      "4.4.0", COMPUTE_CONNECTION_ERRORS_IN_STATS_PROPERTY),
 
   /**
-   * If set to true, managed iterators transformed to Strings will show the representation of the elements instead of the generic
-   * 'org.mule.runtime.core.internal.streaming.object.ManagedCursorIteratorProvider$ManagedCursorIterator@######'.
+   * When enabled, managed cursor iterators transformed to Strings will show the representation of the elements instead of the
+   * generic 'org.mule.runtime.core.internal.streaming.object.ManagedCursorIteratorProvider$ManagedCursorIterator@######'.
    *
    * @since 4.4.0
    */
   TO_STRING_TRANSFORMER_TRANSFORM_ITERATOR_ELEMENTS(
-      "If set to true, managed iterators transformed to Strings will show the representation of the elements instead of the generic 'org.mule.runtime.core.internal.streaming.object.ManagedCursorIteratorProvider$ManagedCursorIterator@######'.",
+      "When enabled, managed cursor iterators transformed to Strings will show the representation of the elements instead of the generic 'org.mule.runtime.core.internal.streaming.object.ManagedCursorIteratorProvider$ManagedCursorIterator@######'.",
       "MULE-19323",
       "4.4.0",
       TO_STRING_TRANSFORMER_TRANSFORM_ITERATOR_ELEMENTS_PROPERTY),
 
   /**
-   * If set to true, extensions imported by a policy will be managed in complete isolation from the extensions imported by the
+   * When enabled, extensions imported by a policy will be managed in complete isolation from the extensions imported by the
    * application that is being applied to, and validations will prevent the usage of explicit configurations declared by the
    * application as part of the policy initialization."
    *
-   * @since 4.4.0, 4.3.1
+   * @since 4.4.0, 4.3.0-202107
    */
   ENABLE_POLICY_ISOLATION(
-      "If set to true, extensions imported by a policy will be managed in complete isolation from the extensions imported by the application that is being applied to, and validations will prevent the usage of explicit configurations declared by the application as part of the policy initialization.",
+      "When enabled, extensions imported by a policy will be managed in complete isolation from the extensions imported by the application that is being applied to, and validations will prevent the usage of explicit configurations declared by the application as part of the policy initialization.",
       "MULE-19226",
-      "4.4.0, 4.3.1", ENABLE_POLICY_ISOLATION_PROPERTY),
+      "4.4.0", ENABLE_POLICY_ISOLATION_PROPERTY),
 
   /**
-   * If set to true, the Mule XML DSL parser will fail when deploying an application that declares a schema that cannot be
-   * located. If set to false, it will only fail if such application makes use of the namespace that such schema defines, instead
-   * of just declaring it.
+   * When enabled, the Mule XML DSL parser will fail when deploying an application that declares a schema that cannot be located.
+   * Otherwise, it will fail if the application also makes use of the namespace that such schema is bound to.
    *
    * @since 4.4.0
    */
   ENTITY_RESOLVER_FAIL_ON_FIRST_ERROR(
-      "If set to true, the Mule XML DSL parser will fail when deploying an application that declares a schema that cannot be located. If set to false, it will only fail if such application makes use of the namespace that such schema defines, instead of just declaring it.",
+      "When enabled, the Mule XML DSL parser will fail when deploying an application that declares a schema that cannot be located. Otherwise, it will fail if the application also makes use of the namespace that such schema is bound to.",
       "EE-7827",
       "4.4.0",
       ENTITY_RESOLVER_FAIL_ON_FIRST_ERROR_PROPERTY),
 
   /**
-   * If set to true, profiling events will be produced.
+   * When enabled, runtime profiling capabilities will become available.
    *
    * @since 4.4.0
    */
   ENABLE_PROFILING_SERVICE(
-      "If set to true, profiling events will be produced",
+      "When enabled, runtime profiling capabilities will become available.",
       "MULE-19588",
       "4.5.0",
       ENABLE_PROFILING_SERVICE_PROPERTY),
 
+  /**
+   * When enabled, the Set Variable component will create a variable even if it's value is null.
+   *
+   * @since 4.4.0
+   */
   SET_VARIABLE_WITH_NULL_VALUE(
-      "If set to true, the set variable will create a variable even if the value is null",
+      "When enabled, the Set Variable component will create a variable even if it's value is null.",
       "MULE-19443",
       "4.4.0",
       SET_VARIABLE_WITH_NULL_VALUE_PROPERTY);
 
   private final String description;
   private final String issueId;
-  private final String since;
+  private final String enabledByDefaultSince;
   private final String overridingSystemPropertyName;
 
-  MuleRuntimeFeature(String description, String issue, String since) {
-    this(description, issue, since, null);
-  }
-
-  MuleRuntimeFeature(String description, String issueId, String since, String overridingSystemPropertyName) {
+  MuleRuntimeFeature(String description, String issueId, String enabledByDefaultSince, String overridingSystemPropertyName) {
     this.description = description;
     this.issueId = issueId;
-    this.since = since;
+    this.enabledByDefaultSince = enabledByDefaultSince;
     this.overridingSystemPropertyName = overridingSystemPropertyName;
   }
 
@@ -184,7 +176,12 @@ public enum MuleRuntimeFeature implements Feature {
 
   @Override
   public String getSince() {
-    return since;
+    return getEnabledByDefaultSince();
+  }
+
+  @Override
+  public String getEnabledByDefaultSince() {
+    return enabledByDefaultSince;
   }
 
   @Override
