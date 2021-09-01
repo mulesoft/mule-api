f8207501f33b874075c4c41bfc4554abda1c8fff
Ivan Fritzler <ifritzler@mulesoft.com>
2021-09-01 01:39:35 -0300

    MULE-19596: Implement a profiling data consumer discovery strategy based on server plugins

--- src/main/java/org/mule/runtime/api/profiling/ProfilingDataConsumerDiscoveryStrategy.java
+++ src/main/java/org/mule/runtime/api/profiling/ProfilingDataConsumerDiscoveryStrategy.java
@@ -29,6 +29,6 @@ public interface ProfilingDataConsumerDiscoveryStrategy {
    * @param <T> the type for the event context which will have the profiling event context data.
    * @return the discovered {@link ProfilingDataProducer}'s.
    */
-  <S extends ProfilingDataConsumer<T>, T extends ProfilingEventContext> Set<S> discover();
+  Set<ProfilingDataConsumer<? extends ProfilingEventContext>> discover();
 
 }
