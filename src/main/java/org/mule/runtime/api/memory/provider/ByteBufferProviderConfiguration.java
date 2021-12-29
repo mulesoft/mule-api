257534102d2d13f09d1977895c0273b900a543ab
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2021-12-29 17:42:09 -0300

    EE-8095: Create a container-scoped memory management service (#924)

---
+++ src/main/java/org/mule/runtime/api/memory/provider/ByteBufferProviderConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.memory.provider;
+
+
+import org.mule.runtime.api.memory.provider.type.ByteBufferType;
+
+/**
+ * A Configuration for a {@link ByteBufferProvider}
+ */
+public interface ByteBufferProviderConfiguration {
+
+  /**
+   * @return the type of buffer a {@link ByteBufferProvider} returns.
+   */
+  ByteBufferType getByteBufferType();
+
+
+  /**
+   * @return the configuration for the created pools.
+   */
+  ByteBufferPoolConfiguration getByteBufferPoolConfiguration();
+
+}
