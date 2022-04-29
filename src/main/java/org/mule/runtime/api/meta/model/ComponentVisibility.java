361e0a984b9fbdf94f8954cfcf5d14445b85604b
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-04-29 10:47:16 -0300

    javadoc

--- src/main/java/org/mule/runtime/api/meta/model/ComponentVisibility.java
+++ src/main/java/org/mule/runtime/api/meta/model/ComponentVisibility.java
@@ -1,5 +1,16 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.api.meta.model;
 
+/**
+ * Defines the visibility of a given component.
+ *
+ * @since 1.5
+ */
 public enum ComponentVisibility {
 
   PRIVATE, PUBLIC;
