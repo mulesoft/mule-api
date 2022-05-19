98880c78143e7a31202e235add0250f572b84792
Ayelen Lopez Bizon <ayelen.lopez@mulesoft.com>
2022-05-19 13:00:13 -0300

    Remove unnecessary import.

--- src/main/java/org/mule/runtime/api/component/location/Location.java
+++ src/main/java/org/mule/runtime/api/component/location/Location.java
@@ -9,7 +9,6 @@ package org.mule.runtime.api.component.location;
 import static org.mule.runtime.api.util.Preconditions.checkState;
 import static org.mule.runtime.internal.util.NameValidationUtil.verifyStringDoesNotContainsReservedCharacters;
 
-import static java.lang.Boolean.FALSE;
 import static java.lang.String.join;
 
 import static org.apache.commons.lang3.StringUtils.isNumeric;
