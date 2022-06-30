1080a0f2f9f468bd88fbafabd598e8a9b3f23d33
TomoDC <TomoDC@users.noreply.github.com>
2022-06-30 15:32:47 -0300

    W-11302687: Add getAppId and getMinMuleVersion to the ExpressionLanguageConfiguration (#999)

--- src/main/java/org/mule/runtime/api/el/ExpressionLanguageConfigurationBuilder.java
+++ src/main/java/org/mule/runtime/api/el/ExpressionLanguageConfigurationBuilder.java
@@ -9,14 +9,20 @@ package org.mule.runtime.api.el;
 import static java.nio.charset.Charset.defaultCharset;
 
 import org.mule.runtime.api.config.FeatureFlaggingService;
+import org.mule.runtime.api.meta.MuleVersion;
 
 import java.nio.charset.Charset;
+import java.util.Optional;
 
 public class ExpressionLanguageConfigurationBuilder {
 
   private Charset charset = defaultCharset();
 
   private FeatureFlaggingService featureFlaggingService;
+
+  private String appId;
+
+  private Optional<MuleVersion> minMuleVersion;
 
   ExpressionLanguageConfigurationBuilder() {}
 
@@ -30,8 +36,18 @@ public class ExpressionLanguageConfigurationBuilder {
     return this;
   }
 
+  public ExpressionLanguageConfigurationBuilder appId(String id) {
+    this.appId = id;
+    return this;
+  }
+
+  public ExpressionLanguageConfigurationBuilder minMuleVersion(Optional<MuleVersion> minMuleVersion) {
+    this.minMuleVersion = minMuleVersion;
+    return this;
+  }
+
   public ExpressionLanguageConfiguration build() {
-    return new DefaultExpressionLanguageConfiguration(charset, featureFlaggingService);
+    return new DefaultExpressionLanguageConfiguration(charset, featureFlaggingService, appId, minMuleVersion);
   }
 
 }
