550a11d7e7cc5e32276636a3a207258e17817a99
sofiamorseletto <sofimorseletto@gmail.com>
2022-06-30 10:24:14 -0300

    Fixed property name

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -553,7 +553,7 @@ public final class MuleSystemProperties {
    * @since 4.5.0
    */
   public static final String HONOR_ERROR_MAPPINGS_WHEN_POLICY_APPLIED_ON_OPERATION_PROPERTY =
-      SYSTEM_PROPERTY_PREFIX + "applyOperationErrorMappingsWhenInsideOperationPolicy";
+      SYSTEM_PROPERTY_PREFIX + "honorErrorMappingsWhenPolicyAppliedOnOperation";
 
 
   /**
