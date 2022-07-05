bd8e53e0af316ac0c3fed7cca2ff0ee1fc866e1a
sofiamorseletto <sofimorseletto@gmail.com>
2022-07-05 17:07:49 -0300

    Changing property name

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -552,8 +552,8 @@ public final class MuleSystemProperties {
    *
    * @since 4.5.0
    */
-  public static final String HONOR_ERROR_MAPPINGS_WHEN_POLICY_APPLIED_ON_OPERATION_PROPERTY =
-      SYSTEM_PROPERTY_PREFIX + "honorErrorMappingsWhenPolicyAppliedOnOperation";
+  public static final String HONOUR_ERROR_MAPPINGS_WHEN_POLICY_APPLIED_ON_OPERATION_PROPERTY =
+      SYSTEM_PROPERTY_PREFIX + "honourErrorMappingsWhenPolicyAppliedOnOperation";
 
   /**
    * When set to true, it disables JDK vendor validation in JdkVersionUtils
