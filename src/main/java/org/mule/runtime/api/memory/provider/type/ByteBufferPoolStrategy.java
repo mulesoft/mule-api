947d181b846522eebf1e239b72f44b15d3c274de
TomoDC <ch.chibana@gmail.com>
2022-07-26 19:18:03 -0300

    Rename pool strategy

--- src/main/java/org/mule/runtime/api/memory/provider/type/ByteBufferPoolStrategy.java
+++ src/main/java/org/mule/runtime/api/memory/provider/type/ByteBufferPoolStrategy.java
@@ -19,12 +19,13 @@ import org.mule.runtime.api.memory.provider.ByteBufferProvider;
 public enum ByteBufferPoolStrategy {
 
   /**
-   * Default thread pool based byte buffer provider.
+   * Default thread pool based byte buffer provider. The pool contains buffers of an incremental size and allocates chunks of
+   * them.
    */
-  DEFAULT,
+  CHUNKED_BUFFERS_POOL,
 
   /**
-   * Pool strategy based on the DataWeave one.
+   * Pool strategy based on the DataWeave one. The pool saves released DirectByteBuffers of a fixed size to be reused.
    */
-  DW
+  FIXED_BUFFERS_POOL
 }
