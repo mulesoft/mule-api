0b01f012d87b4862511758f749ad3888b2a95772
TomoDC <TomoDC@users.noreply.github.com>
2022-07-27 15:49:25 -0300

    W-11425552: Add pool strategy in the ByteBufferPoolConfiguration (#1013)

--- src/main/java/org/mule/runtime/api/el/ExpressionLanguage.java
+++ src/main/java/org/mule/runtime/api/el/ExpressionLanguage.java
@@ -6,6 +6,7 @@
  */
 package org.mule.runtime.api.el;
 
+import org.mule.runtime.api.lifecycle.Disposable;
 import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.api.metadata.MediaType;
 import org.mule.runtime.api.metadata.TypedValue;
@@ -20,7 +21,7 @@ import java.util.Optional;
  *
  * @since 1.0
  */
-public interface ExpressionLanguage {
+public interface ExpressionLanguage extends Disposable {
 
   /**
    * Includes the bindings in a given {@link BindingContext} as global ones, that should not change often and should be considered
@@ -152,6 +153,13 @@ public interface ExpressionLanguage {
       }
     };
   }
+
+  /**
+   * @since 1.5.0
+   */
+  default void dispose() {
+    // Nothing to do
+  };
 
   /**
    * Returns an object that caches computation results. Provides better performance when evaluation multiple expressions on the
---
+++ src/main/java/org/mule/runtime/api/memory/provider/type/ByteBufferPoolStrategy.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.memory.provider.type;
+
+import org.mule.api.annotation.Experimental;
+import org.mule.runtime.api.memory.provider.ByteBufferProvider;
+
+
+/**
+ * Represents the pooling strategy that a {@link ByteBufferProvider} will use.
+ *
+ * @since 4.5.0
+ */
+@Experimental
+public enum ByteBufferPoolStrategy {
+
+  /**
+   * Default thread pool based byte buffer provider. The pool contains buffers of an incremental size and allocates chunks of
+   * them.
+   */
+  CHUNKED_BUFFERS_POOL,
+
+  /**
+   * Pool strategy based on the DataWeave one. The pool saves released DirectByteBuffers of a fixed size to be reused.
+   */
+  FIXED_BUFFERS_POOL
+}
