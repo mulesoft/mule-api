4ad0bef5fabde190a29e1bc8c901c215c9883ed4
Mariano Rean <mrean@mulesoft.com>
2022-10-13 15:31:55 -0300

    W-11861058: Allow obtaining the failing component from a PipelineMessageNotification (#1046)
    (cherry picked from commit c6fb104e3e3574b66464c2b68db4e171de688fec)

--- src/main/java/org/mule/runtime/api/notification/PipelineMessageNotification.java
+++ src/main/java/org/mule/runtime/api/notification/PipelineMessageNotification.java
@@ -6,12 +6,27 @@
  */
 package org.mule.runtime.api.notification;
 
+import static java.lang.String.format;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.mule.runtime.api.component.Component;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+
 /**
  * <code>PipelineMessageNotification</code> is fired at key steps in the processing of {@link Pipeline}
  */
 public final class PipelineMessageNotification extends EnrichedServerNotification {
 
   private static final long serialVersionUID = 6065691696506216248L;
+
+  private static final Logger LOGGER = getLogger(PipelineMessageNotification.class);
 
   // Fired when processing of pipeline starts
   public static final int PROCESS_START = PIPELINE_MESSAGE_EVENT_ACTION_START_RANGE + 1;
@@ -42,5 +57,24 @@ public final class PipelineMessageNotification extends EnrichedServerNotificatio
   @Override
   public String getEventName() {
     return "PipelineMessageNotification";
+  }
+
+  /**
+   * Get the failing component if the pipeline has failed.
+   *
+   * @return An optional with the failing component.
+   */
+  public Optional<Component> getFailingComponent() {
+    if (getException() == null) {
+      return empty();
+    }
+    try {
+      return of((Component) getException().getClass().getMethod("getFailingComponent").invoke(getException()));
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | ClassCastException e) {
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug(format("Error accessing failing component for: %s", getException()), e);
+      }
+      return empty();
+    }
   }
 }
