6a2d851d154079052ac987a37d45a08a9f355203
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2022-11-22 15:06:33 -0300

    W-12074281: Change ApplicationTypeLoader

---
+++ src/main/java/org/mule/runtime/api/metadata/ArtifactTypeLoader.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.metadata;
+
+import org.mule.api.annotation.NoImplement;
+import org.mule.metadata.api.TypeLoader;
+
+/**
+ * A {@link TypeLoader} that can load all the types of an artifact.
+ *
+ * @since 1.5
+ */
+@NoImplement
+public interface ArtifactTypeLoader extends TypeLoader {
+}
