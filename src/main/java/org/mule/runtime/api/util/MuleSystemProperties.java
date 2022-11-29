090b65f000e77eda99f5bcd0ef2754ec78067eb7
Axel Sanguinetti <asanguinetti@mulesoft.com>
2022-11-29 11:02:08 -0300

    Revert "Inconsistent behaviour when using insecure=true attribute of trust-store config (#1039)"
    This reverts commit c04de13c5f796067e83915ec37616c511bfd6d28.

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -637,14 +637,6 @@ public final class MuleSystemProperties {
   public static final String SUPPRESS_ERRORS_PROPERTY = SYSTEM_PROPERTY_PREFIX + "suppress.mule.exceptions";
 
   /**
-   * When enabled, the insecure attribute of the trust-store element will be honoured even when other attributes are configured.
-   *
-   * @since 4.5.0
-   */
-  public static final String HONOUR_INSECURE_TLS_CONFIGURATION_PROPERTY =
-      SYSTEM_PROPERTY_PREFIX + "honour.insecure.tls.configuration";
-
-  /**
    * When set to {@code true}, {@link ExtensionModel} discovery and loading will happen in parallel.
    * <p>
    * This feature is <b>EXPERIMENTAL</b>
