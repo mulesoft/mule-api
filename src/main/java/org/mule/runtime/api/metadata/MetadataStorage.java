83a965312a5052cb174435b73b466eb1234fa808
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-12-15 10:50:27 -0300

    W-12181023: Not extending API and adding Adapter:

--- src/main/java/org/mule/runtime/api/metadata/MetadataStorage.java
+++ src/main/java/org/mule/runtime/api/metadata/MetadataStorage.java
@@ -7,20 +7,62 @@
 package org.mule.runtime.api.metadata;
 
 import org.mule.api.annotation.NoImplement;
+import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.metadata.resolving.InputTypeResolver;
 import org.mule.runtime.api.metadata.resolving.OutputTypeResolver;
 import org.mule.runtime.api.metadata.resolving.TypeKeysResolver;
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Optional;
 
 /**
- * Like {@link MetadataCache}, this component provides the capability to store data so future requests for that data can be served
- * faster and accessed from every {@link InputTypeResolver}, {@link TypeKeysResolver} and {@link OutputTypeResolver}. It also
- * allows to evict entries in case recalculations are needed.
+ * This component provides the capability to store data so future requests for that data can be served faster and accessed from
+ * every {@link InputTypeResolver}, {@link TypeKeysResolver} and {@link OutputTypeResolver}. It also allows to evict entries in
+ * case recalculations are needed.
  * 
  * @since 1.5
  */
 @NoImplement
-public interface MetadataStorage extends MetadataCache {
+public interface MetadataStorage {
+
+  /**
+   * Associates the specified value with the specified key in the storage. if the storage previously contained a mapping for the
+   * specified key, the old value gets replaced
+   *
+   * @param key   a key to associate the specified value
+   * @param value the value to persist in the storage
+   */
+  void put(Serializable key, Serializable value);
+
+  /**
+   * Copies all of the entries from the specified map to the storage.
+   *
+   * @param values values to be stored in the storage
+   */
+  void putAll(Map<? extends Serializable, ? extends Serializable> values);
+
+  /**
+   * @param key the key whose associated value is to be returned
+   * @return the value to which the specified key is mapped, or {@code Option.empty()} if this map contains no value for the
+   *         specified key.
+   */
+  <T extends Serializable> Optional<T> get(Serializable key);
+
+  /**
+   * If the specified key is not already associated with a value, attempts to compute its value using the given mapping function
+   * and enters it into this map unless {@code null}.
+   *
+   * @param key the key whose associated value is to be returned
+   * @return the current (existing or computed) value associated with the specified key, or null if the computed value is null
+   */
+  <T extends Serializable> T computeIfAbsent(Serializable key, MetadataStorageValueResolver mappingFunction)
+      throws MetadataResolvingException, ConnectionException;
+
+  @FunctionalInterface
+  interface MetadataStorageValueResolver {
+
+    Serializable compute(Serializable key) throws MetadataResolvingException, ConnectionException;
+  }
 
   /**
    * Evicts the key in the storage.
---
+++ src/main/java/org/mule/runtime/api/metadata/MetadataStorageToCacheAdapter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.metadata;
+
+import org.mule.runtime.api.connection.ConnectionException;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Adapter to go from {@link MetadataStorage} to {@link MetadataCache} to use in the APIs that are still depending
+ * on {@link MetadataCache}.
+ *
+ * @since 1.5.
+ */
+public class MetadataStorageToCacheAdapter implements MetadataCache {
+
+  private MetadataStorage delegate;
+
+  public MetadataStorageToCacheAdapter(MetadataStorage storage) {
+    this.delegate = storage;
+  }
+
+  @Override
+  public void put(Serializable key, Serializable value) {
+    this.delegate.put(key, value);
+  }
+
+  @Override
+  public void putAll(Map<? extends Serializable, ? extends Serializable> values) {
+    this.putAll(values);
+  }
+
+  @Override
+  public <T extends Serializable> Optional<T> get(Serializable key) {
+    return this.delegate.get(key);
+  }
+
+  @Override
+  public <T extends Serializable> T computeIfAbsent(Serializable key, MetadataCacheValueResolver mappingFunction)
+      throws MetadataResolvingException, ConnectionException {
+    return this.delegate.computeIfAbsent(key, mappingFunction::compute);
+  }
+}
