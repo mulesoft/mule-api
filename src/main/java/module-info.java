2f91969685ae0eb066026b70df119ef73caea11d
elrodro83 <rmerino@salesforce.com>
2023-04-12 17:46:03 -0300

    W-12980682: Modularize dsl-api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -114,6 +114,9 @@ module org.mule.runtime.api {
   exports org.mule.runtime.api.util.xmlsecurity;
   exports org.mule.runtime.api.value;
   
+  exports org.mule.runtime.internal.dsl to
+      org.mule.runtime.dsl.api;
+  
   // Allow introspection for serialization/deserialization by Gson
   opens org.mule.runtime.api.component to
       com.google.gson;
