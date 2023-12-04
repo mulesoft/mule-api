924fa4b91f8b34355a3ebc4579a4874daf425dbc
rbourbonnavarro <rodrigobourbon44@gmail.com>
2023-12-04 12:16:31 -0300

    Review.

--- src/main/java/org/mule/runtime/api/config/custom/CustomizationService.java
+++ src/main/java/org/mule/runtime/api/config/custom/CustomizationService.java
@@ -9,8 +9,7 @@ package org.mule.runtime.api.config.custom;
 import org.mule.api.annotation.NoImplement;
 import org.mule.runtime.api.lifecycle.Lifecycle;
 
-import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 /**
  * Interface that allows to customize the set of services provided by the {@code MuleContext}.
@@ -40,18 +39,44 @@ public interface CustomizationService {
   <T> void overrideDefaultServiceImpl(String serviceId, T serviceImpl);
 
   /**
-   * Allows to override a service provided by default on a mule context by decorating it.
+   * Allows to override a service provided by default on a mule context.
    * <p>
-   * The service implementation resulting from the decoration can be annotated with @Inject and implement methods from
-   * {@link Lifecycle}.
+   * The new service implementation can be annotated with {@code @Inject} and implement methods from {@link Lifecycle}.
    * <p>
    * The service identifier can be used to locate the service in the mule registry.
    *
-   * @param serviceId        identifier of the service implementation to customize.
-   * @param serviceDecorator the decorator to apply on the default service implementation.
+   * @param serviceId        identifier of the service implementation to override.
+   * @param serviceOverrider a {@link Consumer} that will use a {@link ServiceOverrider} to override the default service
+   *                         implementation.
    * @since 4.6
    */
-  void decorateDefaultServiceImpl(String serviceId, Function<Object, Optional<Object>> serviceDecorator);
+  void overrideDefaultServiceImpl(String serviceId, Consumer<ServiceOverrider> serviceOverrider);
+
+  /**
+   * Handles the override of a service implementation, which means either using a different one or flagging it to be removed.
+   *
+   * @since 4.6
+   */
+  interface ServiceOverrider {
+
+    /**
+     * @return the default service implementation that is going to be overridden.
+     */
+    Object getOverridee();
+
+    /**
+     * Sets the new service implementation that overrides the default one.
+     * 
+     * @param overrider the new service implementation.
+     */
+    void override(Object overrider);
+
+    /**
+     * Flags that no implementation of the current service must be added to the registry.
+     */
+    void remove();
+
+  }
 
   /**
    * Allows to override a service provided by default on a mule context. The provided class will be used to instantiate the
