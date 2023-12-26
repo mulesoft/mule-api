0c2c2994a5cbc045bbd03935be8269b79b9e72fa
Hyeran Park <hyeran.park@mulesoft.com>
2023-12-26 09:40:49 -0800

    W-14490182: Set maximum cache size for MediaType to prevent attacks/OOM issue

--- src/test/java/org/mule/runtime/api/test/metadata/MediaTypeTestCase.java
+++ src/test/java/org/mule/runtime/api/test/metadata/MediaTypeTestCase.java
@@ -345,35 +345,12 @@ public class MediaTypeTestCase {
   }
 
   @Test
-  public void constantsAndParsedSameInstance() {
-    final MediaType appXml = APPLICATION_XML;
-    assertThat(MediaType.parse(appXml.toRfcString()), sameInstance(appXml));
-  }
-
-  @Test
-  public void sameParsedWithParamsNotSameInstance() {
-    final MediaType withParam1 = MediaType.parse("multipart/lalala; boundary=\"---- next message ----\"");
-    final MediaType withParam2 = MediaType.parse("multipart/lalala; boundary=\"---- next message ----\"");
-
-    assertThat(withParam1, not(sameInstance(withParam2)));
-  }
-
-  @Test
-  public void withoutParametersCached() {
-    final MediaType withParam = MediaType.parse("multipart/lalala; boundary=\"---- next message ----\"");
-    final MediaType withoutParam = MediaType.parse("multipart/lalala");
-
-    assertThat(withParam.withoutParameters(), sameInstance(withoutParam));
-  }
-
-  @Test
   @Issue("W-14490182")
   public void avoidDoSUsingMimeTypeCaching() {
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < 100; i++) {
       MediaType.parse("attack/" + i);
     }
     assertThat(MediaType.getCacheSize(), is(32));
   }
-
 
 }
