297f9f2b58fd23f85b70eaf309f6ef1935037911
Ezequiel Werner <eze210@gmail.com>
2024-02-06 14:28:00 -0300

    allure

--- src/test/java/org/mule/runtime/api/test/metadata/MediaTypeTestCase.java
+++ src/test/java/org/mule/runtime/api/test/metadata/MediaTypeTestCase.java
@@ -345,12 +345,14 @@ public class MediaTypeTestCase {
   }
 
   @Test
+  @Issue("W-14678431")
   public void constantsAndParsedSameInstance() {
     final MediaType appXml = APPLICATION_XML;
     assertThat(MediaType.parse(appXml.toRfcString()), sameInstance(appXml));
   }
 
   @Test
+  @Issue("W-14678431")
   public void sameParsedWithParamsNotSameInstance() {
     final MediaType withParam1 = MediaType.parse("multipart/lalala; boundary=\"---- next message ----\"");
     final MediaType withParam2 = MediaType.parse("multipart/lalala; boundary=\"---- next message ----\"");
@@ -359,6 +361,7 @@ public class MediaTypeTestCase {
   }
 
   @Test
+  @Issue("W-14678431")
   public void withoutParametersCached() {
     final MediaType withParam = MediaType.parse("multipart/lalala; boundary=\"---- next message ----\"");
     final MediaType withoutParam = MediaType.parse("multipart/lalala");
