637cc0d07fc27cc4a5657830bcd7bbbc502c31e9
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2024-02-27 19:53:25 +0100

    introducing model

---
+++ src/main/java/org/mule/runtime/api/meta/model/nested/ChainExecutionOccurrence.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.api.meta.model.nested;
+
+public enum ChainExecutionOccurrence {
+
+  ONCE_OR_NONE, ONCE, AT_LEAST_ONCE, MULTIPLE_OR_NONE, UNKNOWN
+}
