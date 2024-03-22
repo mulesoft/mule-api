44cc617c524ddbeb466f6da04b40020869f7abf6
rbourbonnavarro <rodrigobourbon44@gmail.com>
2024-03-22 18:01:35 -0300

    Remove `org.mule.runtime.internal.config.custom` and provide `ServiceConfigurator` lookup in API

--- src/main/java/org/mule/runtime/api/config/custom/ServiceConfigurator.java
+++ src/main/java/org/mule/runtime/api/config/custom/ServiceConfigurator.java
@@ -6,10 +6,27 @@
  */
 package org.mule.runtime.api.config.custom;
 
+import static java.util.ServiceLoader.load;
+import static java.util.stream.StreamSupport.stream;
+
 import org.mule.api.annotation.NoImplement;
+
+import java.util.stream.Stream;
 
 @NoImplement
 public interface ServiceConfigurator {
+
+  /**
+   * Looks up implementations of {@link ServiceConfigurator}.
+   *
+   * @return the discovered {@link ServiceConfigurator}s.
+   */
+  static Stream<ServiceConfigurator> lookupServiceConfigurators() {
+    return stream(((Iterable<ServiceConfigurator>) () -> load(ServiceConfigurator.class,
+                                                              ServiceConfigurator.class.getClassLoader())
+                                                                  .iterator()).spliterator(),
+                  false);
+  }
 
   void configure(CustomizationService customizationService);
 }
