b2e956794eb6c05215a7743d9b51cb31d27fd5c2
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2024-03-27 15:22:21 +0100

    self review

--- src/main/java/org/mule/runtime/api/metadata/ChainPropagationContext.java
+++ src/main/java/org/mule/runtime/api/metadata/ChainPropagationContext.java
@@ -6,15 +6,23 @@
  */
 package org.mule.runtime.api.metadata;
 
+import org.mule.api.annotation.Experimental;
 import org.mule.metadata.message.api.MessageMetadataType;
 
 import java.util.function.Supplier;
 
 /**
- * Models the propagation context of a Scope.
+ * Contains DataSense propagation information by using a {@link MessageMetadataType} to describe
+ * the typed message that enters a {@code Chain} (input) and the message that exits it (output)
+ * <p>
+ * This context represents chains generically. It can be used to describe the single inner chain of a scope, or the inner chain
+ * of the multiple routes in a router component.
+ * <p>
+ * <b>NOTE:</b> Experimental feature. Backwards compatibility is not guaranteed.
  *
- * @since 1.7
+ * @since 1.7.0
  */
+@Experimental
 public interface ChainPropagationContext {
 
   /**
