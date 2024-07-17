1f833e4242ba11d3a9e1bbf225cceb47b05e9a63
Axel Sanguinetti <asanguinetti@mulesoft.com>
2024-07-17 13:48:18 -0300

    Feedback

--- src/main/java/org/mule/runtime/privileged/metadata/InternalMetadataContext.java
+++ src/main/java/org/mule/runtime/privileged/metadata/InternalMetadataContext.java
@@ -6,6 +6,9 @@
  */
 package org.mule.runtime.privileged.metadata;
 
+import org.mule.api.annotation.Experimental;
+import org.mule.api.annotation.NoImplement;
+import org.mule.metadata.api.model.MetadataType;
 import org.mule.metadata.message.api.el.TypeBindings;
 import org.mule.runtime.api.metadata.ExpressionLanguageMetadataService;
 import org.mule.runtime.api.metadata.MetadataContext;
@@ -14,9 +17,13 @@ import java.util.Optional;
 
 /**
  * A {@link MetadataContext} with additional capabilities, for internal use only.
+ * <p>
+ * <b>NOTE:</b> Experimental feature. Backwards compatibility not guaranteed.
  *
  * @since 4.8.0
  */
+@NoImplement
+@Experimental
 public interface InternalMetadataContext extends MetadataContext {
 
   /**
@@ -25,6 +32,13 @@ public interface InternalMetadataContext extends MetadataContext {
   Optional<ExpressionLanguageMetadataService> getExpressionLanguageMetadataService();
 
   /**
+   * Allows for specifying the {@link MetadataType}s of the different bindings provided to the expression language.
+   * <p>
+   * Examples could be payload, attributes, variables, errors and even module definitions.
+   * <p>
+   * This, in combination with the {@link ExpressionLanguageMetadataService}, can be used by a type resolver to provide more
+   * precise information about the types involved in an expression.
+   *
    * @return the {@link TypeBindings} in context, if available.
    */
   Optional<TypeBindings> getTypeBindings();
