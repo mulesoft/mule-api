16e5d9df7ef6f31a3788be965a3aed418f011a6f
Mariano Rean <mrean@mulesoft.com>
2024-07-18 12:07:26 -0300

    W-15750334: Flows with stopped initial state cannot maintain its status when restarting the application

--- src/main/java/org/mule/runtime/api/config/MuleRuntimeFeature.java
+++ src/main/java/org/mule/runtime/api/config/MuleRuntimeFeature.java
@@ -514,9 +514,9 @@ public enum MuleRuntimeFeature implements Feature {
    * @since 4.8
    */
   HONOUR_PERSISTED_FLOW_STATE(
-    "When enabled, flows will honour the state configured in flows.deployment.properties when restarting the app, regardless of the initial state.",
-    "W-15750334",
-    "4.8.0", HONOUR_PERSISTED_FLOW_STATE_PROPERTY);
+      "When enabled, flows will honour the state configured in flows.deployment.properties when restarting the app, regardless of the initial state.",
+      "W-15750334",
+      "4.8.0", HONOUR_PERSISTED_FLOW_STATE_PROPERTY);
 
   private final String description;
   private final String issueId;
