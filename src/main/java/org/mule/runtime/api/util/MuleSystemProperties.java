f93026b911c77753d34ee1d6247cca50de9c1475
Axel Sanguinetti <asanguinetti@salesforce.com>
2025-02-14 17:09:07 -0300

    W-16941297: Adding feature flag for ForJoinRouters to complete their child contexts on timeout.
    (cherry picked from commit f085f4072cbee15e23efd6221665a023a98559e1)

--- src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
+++ src/main/java/org/mule/runtime/api/util/MuleSystemProperties.java
@@ -905,6 +905,15 @@ public final class MuleSystemProperties {
       SYSTEM_PROPERTY_PREFIX + "ntlm.avoid.send.payload.on.type1";
 
   /**
+   * When enabled, the processors that perform fork and join work (currently Scatter Gather and Parallel For Each), will take care
+   * of completing the child event contexts when there is a timeout.
+   *
+   * @since 4.10.0, 4.9.2, 4.6.14, 4.4.0-202503
+   */
+  public static final String FORK_JOIN_COMPLETE_CHILDREN_ON_TIMEOUT_PROPERTY =
+      SYSTEM_PROPERTY_PREFIX + "forkJoin.completeChildContextsOnTimeout";
+
+  /**
    * Returns {@code true} if a {@link java.lang.ModuleLayer} will be used for creating the isolated context for the classes of the
    * container and the services ({@code true}) or a classLoader will be used ({@code false}).
    * <p>
