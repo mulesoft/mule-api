0fa3bc08db34631a230252b5e1f3f2d5a7ebb705
Alejandro Javier Nosenzo <1653997+anosenzo@users.noreply.github.com>
2025-03-28 18:10:03 -0300

    W-18021352: [SPIKE] Build Mule repo using strata

---
+++ download_maven.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e +x
+
+if [ -z ${NEXUS_BASE_URL+x} ]; then
+#	echo "warning: NEXUS_BASE_URL is not set. Setting default value"
+	NEXUS_URL="https://nexus-proxy.repo.local.sfdc.net/nexus/content"
+else
+	NEXUS_URL=${NEXUS_BASE_URL}
+fi
+
+### Remove trailing slash if present
+NEXUS_URL=$(echo "$NEXUS_URL" | sed "s,/$,,")
+
+MAVEN_TAR_FILE_NAME=apache-maven-${MAVEN_VERSION}-bin.tar.gz
+
+MAVEN_TAR_DOWNLOAD_URL=${NEXUS_URL}/repositories/central/org/apache/maven/apache-maven/${MAVEN_VERSION}/${MAVEN_TAR_FILE_NAME}
+
+#echo "#### Download apache-maven binary from Nexus"
+
+curl -sS "$MAVEN_TAR_DOWNLOAD_URL" -u $NEXUS_USERNAME:$NEXUS_PASSWORD -o ${MAVEN_TAR_FILE_NAME}
+# Verify the sha values are correct for what we expect (no tampering)
+#echo $(sha512sum ${MAVEN_TAR_FILE_NAME})
+echo "$MAVEN_TAR_DOWNLOAD_SHA512 ${MAVEN_TAR_FILE_NAME}" | sha512sum -c -
+
+MAVEN_TARGET_BASE_DIR=/usr/local
+tar xzf ${MAVEN_TAR_FILE_NAME} --directory ${MAVEN_TARGET_BASE_DIR}
+
+MAVEN_DIR=$(set -- ${MAVEN_TARGET_BASE_DIR}/apache*; echo $1)
+ln -s ${MAVEN_DIR}/bin/* /usr/bin/
+rm -rf ${MAVEN_TAR_FILE_NAME}
+
+echo ${MAVEN_DIR}
+
