c46efcbdb401c0d95f9090b8a6c1cb5c1ecb8a98
Rodrigo E. Merino <rmerino@salesforce.com>
2025-06-13 22:44:19 -0300

    W-18557890: Add XA support information to the extension model

--- src/main/java/org/mule/runtime/api/meta/model/connection/ConnectionProviderModel.java
+++ src/main/java/org/mule/runtime/api/meta/model/connection/ConnectionProviderModel.java
@@ -10,10 +10,10 @@ package org.mule.runtime.api.meta.model.connection;
 import org.mule.api.annotation.NoImplement;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.api.meta.DescribedObject;
-import org.mule.runtime.api.meta.model.declaration.fluent.HasSemanticTerms;
 import org.mule.runtime.api.meta.NamedObject;
 import org.mule.runtime.api.meta.model.EnrichableModel;
 import org.mule.runtime.api.meta.model.HasExternalLibraries;
+import org.mule.runtime.api.meta.model.declaration.fluent.HasSemanticTerms;
 import org.mule.runtime.api.meta.model.deprecated.DeprecableModel;
 import org.mule.runtime.api.meta.model.display.HasDisplayModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
@@ -23,9 +23,9 @@ import org.mule.runtime.api.meta.model.version.HasMinMuleVersion;
 /**
  * Introspection model for {@link ConnectionProvider} types.
  * <p>
- * Provider models implement the flyweight pattern. This means that a given operation should only be represented by only one
- * instance of this class. Thus, if the same operation is contained by different {@link HasConnectionProviderModels} instances,
- * then each of those containers should reference the same operation model instance.
+ * Provider models implement the flyweight pattern. This means that a given connectionProvider should only be represented by only
+ * one instance of this class. Thus, if the same connectionProvider is contained by different {@link HasConnectionProviderModels}
+ * instances, then each of those containers should reference the same connectionProvider model instance.
  * <p>
  *
  * @since 1.0
@@ -45,4 +45,12 @@ public interface ConnectionProviderModel
    * @return whether this provider supports connectivity testing or not
    */
   boolean supportsConnectivityTesting();
+
+  /**
+   * @return whether this provider connections may participate in an XA transaction.
+   * @since 1.10
+   */
+  default boolean supportsXa() {
+    return false;
+  }
 }
